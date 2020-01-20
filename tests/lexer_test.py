b89571e556ca7a96b0c346601bbfd27ac88449ff
d.garcia <d.garciabfd@gmail.com>
2020-01-20 16:56:31 -0500

    Change Report Formmat

--- tests/lexer_test.py
+++ tests/lexer_test.py
@@ -3,11 +3,11 @@ import os
 from utils import compare_errors
 
 tests_dir = __file__.rpartition('/')[0] + '/lexer/'
-tests = [(tests_dir + file) for file in os.listdir(tests_dir) if file.endswith('.cl')]
+tests = [(file) for file in os.listdir(tests_dir) if file.endswith('.cl')]
 
 @pytest.mark.run(order=1)
 @pytest.mark.lexer
 @pytest.mark.error
 @pytest.mark.parametrize("cool_file", tests)
 def test_lexer_errors(compiler_path, cool_file):
-    compare_errors(compiler_path, cool_file, cool_file[:-3] + '_error.txt')
+    compare_errors(compiler_path, tests_dir + cool_file, cool_file[:-3] + '_error.txt')
