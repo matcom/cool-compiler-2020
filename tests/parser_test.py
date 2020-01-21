dccb2193a84199410126563ee4d781058de66ab1
d.garcia <d.garciabfd@gmail.com>
2020-01-21 16:35:09 -0500

    Update tests marks

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -5,9 +5,9 @@ from utils import compare_errors
 tests_dir = __file__.rpartition('/')[0] + '/parser/'
 tests = [(file) for file in os.listdir(tests_dir) if file.endswith('.cl')]
 
-@pytest.mark.run(order=2)
 @pytest.mark.parser
 @pytest.mark.error
+@pytest.mark.run(order=2)
 @pytest.mark.parametrize("cool_file", tests)
 def test_parser_errors(compiler_path, cool_file):
     compare_errors(compiler_path, tests_dir + cool_file, cool_file[:-3] + '_error.txt')
