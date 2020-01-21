9d132c77dee81c5b96bb0f40310025186222fc85
d.garcia <d.garciabfd@gmail.com>
2020-01-21 16:35:09 -0500

    Update tests marks

--- tests/codegen_test.py
+++ tests/codegen_test.py
@@ -5,11 +5,11 @@ from utils import compare_errors
 tests_dir = __file__.rpartition('/')[0] + '/codegen/'
 tests = [(file) for file in os.listdir(tests_dir) if file.endswith('.cl')]
 
-@pytest.mark.run(order=4)
-@pytest.mark.lexer
-@pytest.mark.parser
-@pytest.mark.semantic
+# @pytest.mark.lexer
+# @pytest.mark.parser
+# @pytest.mark.semantic
 @pytest.mark.ok
+@pytest.mark.run(order=4)
 @pytest.mark.parametrize("cool_file", tests)
 def test_codegen(compiler_path, cool_file):
     compare_errors(compiler_path, tests_dir + cool_file, None)
--- tests/semantic_test.py
+++ tests/semantic_test.py
@@ -5,9 +5,9 @@ from utils import compare_errors
 tests_dir = __file__.rpartition('/')[0] + '/semantic/'
 tests = [(file) for file in os.listdir(tests_dir) if file.endswith('.cl')]
 
-@pytest.mark.run(order=3)
 @pytest.mark.semantic
 @pytest.mark.error
+@pytest.mark.run(order=3)
 @pytest.mark.parametrize("cool_file", [])
 def test_semantic_errors(compiler_path, cool_file):
     compare_errors(compiler_path, tests_dir + cool_file, cool_file[:-3] + '_error.txt')
