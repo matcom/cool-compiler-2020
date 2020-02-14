86bdd0c075e0c7d318eb319a3515d8cac6836df3
Tony <otano0398@gmail.com>
2020-02-14 14:48:28 -0500

    Added String rule

--- lexer/lexer.py
+++ lexer/lexer.py
@@ -104,7 +104,7 @@ class Cool_Lexer(object):
         t.lexer.skip(1)
 
 
-    states = (('COMMENT','exclusive'),)
+    states = (('COMMENT','exclusive'), ('STRING', 'exclusive'))
 
     @TOKEN(r'\(\*')
     def t_begin_COMMENT(self, t):
@@ -124,6 +124,67 @@ class Cool_Lexer(object):
     def t_COMMENT_error(self, t):
         t.lexer.skip(1)
 
+    @TOKEN(r'\"')
+    def t_begin_STRING(self, t):
+        t.lexer.push_state('STRING')
+        t.lexer.string_backslashed = False
+        t.lexer.stringbuf = ''
+        t.lexer.newline_escaped = True
+
+    @TOKEN(r'\n')
+    def t_STRING_newline(self, t):
+        t.lexer.lineno += 1
+        if not t.lexer.newline_escaped:
+            t.lexer.skip(1)
+        elif not t.lexer.string_backslashed:
+            print(f"({t.lexer.lineno - 1}, {self.find_column(t)}) - Lexicographic Error - Newline character not escaped")
+            t.lexer.newline_escaped = False
+            t.lexer.skip(1)
+        else:
+            t.lexer.string_backslashed = False
+
+    @TOKEN(r'\"')
+    def t_STRING_end(self, t):
+        if t.lexer.string_backslashed:
+            t.lexer.stringbuf += '"'
+            t.lexer.string_backslashed = False
+        else:
+            t.lexer.pop_state()
+            if not t.lexer.newline_escaped:
+                t.lexer.skip(1)
+            else:
+                t.type = "STRING"
+                t.value = t.lexer.stringbuf
+                return t
+
+    @TOKEN(r'[^\n]')
+    def t_STRING_line(self, t):
+        if not t.lexer.newline_escaped:
+            t.lexer.skip(1)
+        elif t.lexer.string_backslashed:
+            if t.value == 'b':
+                t.lexer.stringbuf += '\b'
+            elif t.value == 't':
+                t.lexer.stringbuf += '\t'
+            elif t.value == 'n':
+                t.lexer.stringbuf += '\n'
+            elif t.value == 'f':
+                t.lexer.stringbuf += '\f'
+            elif t.value == '\\':
+                t.lexer.stringbuf += '\\'
+            else:
+                t.lexer.stringbuf += t.value
+        else:
+            if t.value == '\\':
+                t.lexer.string_backslashed = True
+            else:
+                t.lexer.stringbuf += t.value
+
+    def t_STRING_error(self, t):
+        print(f'({t.lexer.lineno}, {self.find_column(t)}) - LexicographicError: "{t.value}"')
+        t.lexer.skip(1)
+
+
 if __name__ == "__main__":
     cool_lexer = Cool_Lexer()
     data = ''
@@ -139,4 +200,3 @@ if __name__ == "__main__":
 
     for token in cool_lexer.lexer:
         print(token)
-
