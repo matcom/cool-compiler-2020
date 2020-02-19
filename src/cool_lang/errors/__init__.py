af735e23dd5d8345dc9b7a689d2641ad4488bdcc
leynier <leynier41@gmail.com>
2020-02-18 21:42:32 -0500

    Redesign error hierarchy

--- src/cool_lang/errors/__init__.py
+++ src/cool_lang/errors/__init__.py
@@ -1,33 +1,52 @@
 class Error:
-    def __init__(self, error_name, row, column, description):
+    def __init__(self, error_name, description):
         self.error_name = error_name
+        self.description = description
+
+    def __str__(self):
+        return f'{self.error_name}: {self.description}'
+
+
+class LocalizedError(Error):
+    def __init__(self, error_name, row, column, description):
+        Error.__init__(self, 'SyntacticError', description)
         self.row = row
         self.column = column
-        self.description = description
 
     def __str__(self):
         return f'({self.row},{self.column}) - {self.error_name}: {self.description}'
 
-class LexicographicError(Error):
+
+class LexicographicError(LocalizedError):
     def __init__(self, row, column):
-        Error.__init__(self, 'LexicographicError', row, column, 'Invalid character')
+        LocalizedError.__init__(self, 'LexicographicError', row, column, 'Invalid character')
 
-class SyntacticError(Error):
-    def __init__(self, row, column, description):
-        Error.__init__(self, 'SyntacticError', row, column, description)
 
-class NameError(Error):
+class SyntacticError(LocalizedError):
     def __init__(self, row, column, description):
-        Error.__init__(self, 'NameError', row, column, description)
+        LocalizedError.__init__(self, 'SyntacticError', row, column, description)
 
-class TypeError(Error):
-    def __init__(self, row, column, description):
-        Error.__init__(self, 'TypeError', row, column, description)
 
-class AttributeError(Error):
+class NameError(LocalizedError):
     def __init__(self, row, column, description):
-        Error.__init__(self, 'AttributeError', row, column, description)
+        LocalizedError.__init__(self, 'NameError', row, column, description)
 
-class SemanticError(Error):
+
+class TypeError(LocalizedError):
     def __init__(self, row, column, description):
-        Error.__init__(self, 'SemanticError', row, column, description)        
+        LocalizedError.__init__(self, 'TypeError', row, column, description)
+
+
+class AttributeError(LocalizedError):
+    def __init__(self, row, column, description):
+        LocalizedError.__init__(self, 'AttributeError', row, column, description)
+
+
+class SemanticError(LocalizedError):
+    def LocalizedError(self, row, column, description):
+        LocalizedError.__init__(self, 'SemanticError', row, column, description)        
+
+
+class CompilationError(LocalizedError):
+    def __init__(self, row, column, description):
+        LocalizedError.__init__(self, 'CompilationError', row, column, description)        
