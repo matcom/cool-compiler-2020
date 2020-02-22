1071bc38c33b9f3d681bdf06482bddce30565cf8
Adrian <adriangonzalezsanchez1996@gmail.com>
2020-02-21 23:23:18 -0500

    fixed tilde strings

--- src/coolgrammar/grammar.py
+++ src/coolgrammar/grammar.py
@@ -254,7 +254,7 @@ def build_cool_grammar():
     operators = r"!+|!*|!-|!/|!{|!}|!(|!)|!<|!=|!>|!@|!;|!,|!:|!&|!^|!%|!$|!#|!~"
     quoted_string = r'"(1|2|3|4|5|6|7|8|9|0|A|a|B|b|C|c|D|d|E|e|F|f|G|g|H|h|I|i|J|j|K|k|L|l|M|m|N' + r'|n|O|o|P|p|Q|q|R|r|S|s|T|t|u|U|V|v|W|w|X|x|Y|y|Z|z|! |' + scaped_chars + r'|' + operators + ')*"'
 
-    tilde_string = r"'(1|2|3|4|5|6|7|8|9|0|A|a|B|b|C|c|D|d|E|e|F|f|G|g|H|h|I|i|J|j|K|k|L|l|M|m|N' + r'|n|O|o|P|p|Q|q|R|r|S|s|T|t|u|U|V|v|W|w|X|x|Y|y|Z|z|! |' + scaped_chars + r'|' + operators + ')*'"
+    tilde_string = r"'(1|2|3|4|5|6|7|8|9|0|A|a|B|b|C|c|D|d|E|e|F|f|G|g|H|h|I|i|J|j|K|k|L|l|M|m|N' + r'|n|O|o|P|p|Q|q|R|r|S|s|T|t|u|U|V|v|W|w|X|x|Y|y|Z|z|! |" + scaped_chars + r'|' + operators + ")*'"
 
     table = [
         (class_keyword, 'class'),
