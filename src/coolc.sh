f3c3250641bad12c2cd73ba4f149dc175be8d402
stdevAdrianPaez <adriantpaez@gmail.com>
2020-02-24 16:56:18 -0500

    Delete the echo line in the cool.sh file because the tests need errors to start on line 2 output

--- src/coolc.sh
+++ src/coolc.sh
@@ -4,7 +4,7 @@ INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
-echo "COOL COMPILER 0.0v"
+# echo "COOL COMPILER 0.0v"
 echo "Copyright (c) 2019: Isabella Maria Sierra Ponce, Adrian Tubal Paez Ruiz, Eric Martin Garcia"
 
 # Llamar al compilador
