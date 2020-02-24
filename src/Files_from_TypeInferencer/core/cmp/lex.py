23ac924356e4da9efe573a3f33495b1d5a92f4eb
Mauricio Perdomo <ml.perdomo97@gmail.com>
2020-02-24 18:10:54 -0500

    Change lexer to be a type and add states to it for comments

--- src/Files_from_TypeInferencer/core/cmp/lex.py
+++ src/Files_from_TypeInferencer/core/cmp/lex.py
@@ -3,198 +3,275 @@ import re
 from .utils import Token
 from .CoolUtils import *
 
-__lexer__ = None
-__text__ = None
-__errors__ = None
+class CoolLexer:
 
-reserved = {
-    "class":               "CLASS",          
-    "else":                "ELSE",       
-    "fi":                  "FI",            
-    "if":                  "IF",         
-    "in":                  "IN",         
-    "inherits":            "INHERITS",   
-    "isvoid":              "ISVOID",     
-    "let":                 "LET",        
-    "loop":                "LOOP",       
-    "pool":                "POOL",       
-    "then":                "THEN",       
-    "while":               "WHILE",          
-    "case":                "CASE",       
-    "esac":                "ESAC",       
-    "new":                 "NEW",            
-    "of":                  "OF",         
-    "not":                 "NOT",            
-    "true":                "TRUE",
-    "false":               "FALSE",        
-}
+    states = (
+        ('comments', 'inclusive'),
+    )
 
-tokenType = {
-    "CLASS":        classx,
-    "ELSE":         elsex,
-    "FI":           fi,
-    "IF":           ifx,
-    "IN":           inx,
-    "INHERITS":     inherits,
-    "ISVOID":       isvoid,
-    "LET":          let,
-    "LOOP":         loop,
-    "POOL":         pool,    
-    "THEN":         then,    
-    "WHILE":        whilex,
-    "CASE":         case,    
-    "ESAC":         esac,    
-    "NEW":          new,     
-    "OF":           of,      
-    "NOT":          notx,
-    "OBJECTIDENTIFIER":   idx,
-    "TYPEIDENTIFIER": typex,
-    "LCBRA":        ocur,
-    "RCBRA":        ccur,
-    "LPAREN":       opar,
-    "RPAREN":       cpar,
-    "COLON":        colon,
-    "SEMICOLON":    semi, 
-    "NUMBER":       integer,    
-    "eof":          eof,
-    "PLUS":         plus,
-    "MINUS":        minus,
-    "DIVIDE":       div,
-    "TIMES":        star,
-    "LESS":         less,
-    "LESSEQ":       leq,
-    "EQUALS":       equal,
-    "TRUE":         boolx,
-    "FALSE":        boolx,  
-    "COMPLEMENT":   compl,
-    "RARROW":       rarrow,
-    "LARROW":       larrow,
-    "COMMA":        comma,
-    "DOT":          dot,
-    "AT":           at,
-}
+    reserved = {
+        "class":               "CLASS",          
+        "else":                "ELSE",       
+        "fi":                  "FI",            
+        "if":                  "IF",         
+        "in":                  "IN",         
+        "inherits":            "INHERITS",   
+        "isvoid":              "ISVOID",     
+        "let":                 "LET",        
+        "loop":                "LOOP",       
+        "pool":                "POOL",       
+        "then":                "THEN",       
+        "while":               "WHILE",          
+        "case":                "CASE",       
+        "esac":                "ESAC",       
+        "new":                 "NEW",            
+        "of":                  "OF",         
+        "not":                 "NOT",            
+        "true":                "TRUE",
+        "false":               "FALSE",        
+    }
+
+    tokenType = {
+        "CLASS":        classx,
+        "ELSE":         elsex,
+        "FI":           fi,
+        "IF":           ifx,
+        "IN":           inx,
+        "INHERITS":     inherits,
+        "ISVOID":       isvoid,
+        "LET":          let,
+        "LOOP":         loop,
+        "POOL":         pool,    
+        "THEN":         then,    
+        "WHILE":        whilex,
+        "CASE":         case,    
+        "ESAC":         esac,    
+        "NEW":          new,     
+        "OF":           of,      
+        "NOT":          notx,
+        "OBJECTIDENTIFIER":   idx,
+        "TYPEIDENTIFIER": typex,
+        "LCBRA":        ocur,
+        "RCBRA":        ccur,
+        "LPAREN":       opar,
+        "RPAREN":       cpar,
+        "COLON":        colon,
+        "SEMICOLON":    semi, 
+        "NUMBER":       integer,    
+        "eof":          eof,
+        "PLUS":         plus,
+        "MINUS":        minus,
+        "DIVIDE":       div,
+        "TIMES":        star,
+        "LESS":         less,
+        "LESSEQ":       leq,
+        "EQUALS":       equal,
+        "TRUE":         boolx,
+        "FALSE":        boolx,  
+        "COMPLEMENT":   compl,
+        "RARROW":       rarrow,
+        "LARROW":       larrow,
+        "COMMA":        comma,
+        "DOT":          dot,
+        "AT":           at,
+    }
 
 
-tokens = [
-    'NUMBER',
-    'TYPEIDENTIFIER',
-    'OBJECTIDENTIFIER',
-    'EQUALS',
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
-    'LPAREN',
-    'RPAREN',
-    'STRING',
-    'LESS',
-    'LESSEQ',
-    'LCBRA',
-    'RCBRA',
-    'COLON',
-    'SEMICOLON',
-    'COMPLEMENT',
-    'RARROW',
-    'LARROW',
-    'COMMA',
-    'DOT',
-    'AT'
+    tokens = [
+        'NUMBER',
+        'TYPEIDENTIFIER',
+        'OBJECTIDENTIFIER',
+        'EQUALS',
+        'PLUS',
+        'MINUS',
+        'TIMES',
+        'DIVIDE',
+        'LPAREN',
+        'RPAREN',
+        'STRING',
+        'LESS',
+        'LESSEQ',
+        'LCBRA',
+        'RCBRA',
+        'COLON',
+        'SEMICOLON',
+        'COMPLEMENT',
+        'RARROW',
+        'LARROW',
+        'COMMA',
+        'DOT',
+        'AT'
+
+    ] + list(reserved.values())
+
+    t_EQUALS        = r'=' 
+    t_PLUS          = r'\+'
+    t_MINUS         = r'-'
+    t_TIMES         = r'\*'
+    t_DIVIDE        = r'/'
+    t_LPAREN        = r'\('
+    t_RPAREN        = r'\)'
+    t_LESS          = r'<'
+    t_LESSEQ        = r'<='
+    t_LCBRA         = r'{'
+    t_RCBRA         = r'}'
+    t_COLON         = r':'
+    t_SEMICOLON     = r';'
+    t_COMPLEMENT    = r'~'
+    t_RARROW        = r'=>'
+    t_LARROW        = r'<-'
+    t_COMMA         = r','
+    t_DOT           = r'\.'
+    t_AT            = r'@'
+
+    t_ignore = ' \t\f\r\t\v'
+
+    def build(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+        self.lexer.eof= (1,1)
+        self.lexer.comment_level = 0
+        self.errors = []
+
+    # def t_comments_EQUALS(self, t):
+    #     r'='
+        
+    # def t_comments_PLUS(self, t):
+    #     r'\+'
+             
+    # def t_comments_MINUS(self, t):
+    #     r'-'
+              
+    # def t_comments_TIMES(self, t):
+    #     r'\*'
+                
+    # def t_comments_DIVIDE(self, t):
+    #     r'/'
+                
+    # def t_comments_LPAREN(self, t):
+    #     r'\('
+                 
+    # def t_comments_RPAREN(self, t):
+    #     r'\)'
+                 
+    # def t_comments_LESS(self, t):
+    #     r'<'
+                 
+    # def t_comments_LESSEQ(self, t):
+    #     r'<='
+              
+    # def t_comments_LCBRA(self, t):
+    #     r'{'
+                 
+    # def t_comments_RCBRA(self, t):
+    #     r'}'
+
+    # def t_comments_COLON(self, t):
+    #     r':'
+               
+    # def t_comments_SEMICOLON(self, t):
+    #     r';'
+              
+    # def t_comments_COMPLEMENT(self, t):
+    #     r'~'
+              
+    # def t_comments_RARROW(self, t):
+    #     r'=>'
+              
+    # def t_comments_LARROW(self, t):
+    #     r'<-'
+               
+    # def t_comments_COMMA(self, t):
+    #     r','
+                  
+    # def t_comments_DOT(self, t):
+    #     r'\.'
+      
+    # def t_comments_AT(self, t):
+    #     r'@'
     
-] + list(reserved.values())
-
-t_EQUALS        = r'=' 
-t_PLUS          = r'\+'
-t_MINUS         = r'-'
-t_TIMES         = r'\*'
-t_DIVIDE        = r'/'
-t_LPAREN        = r'\('
-t_RPAREN        = r'\)'
-t_LESS          = r'<'
-t_LESSEQ        = r'<='
-t_LCBRA         = r'{'
-t_RCBRA         = r'}'
-t_COLON         = r':'
-t_SEMICOLON     = r';'
-t_COMPLEMENT    = r'~'
-t_RARROW        = r'=>'
-t_LARROW        = r'<-'
-t_COMMA         = r','
-t_DOT           = r'\.'
-t_AT            = r'@'
-
-t_ignore = ' \t\f\r\t\v'
-
-
-
-def t_TYPEIDENTIFIER(t):
-    r'[A-Z][a-zA-Z0-9|_]*'
-    l_value = t.value.lower()
-    if l_value == "false" or l_value == "true":
-        if t.value[0] != "f" and t.value[0] != 't':
-            return t
-    t.type = reserved.get(l_value, "TYPEIDENTIFIER")
-    return t
-
-def t_OBJECTIDENTIFIER(t):
-    r'[a-z][a-zA-Z0-9|_]*'
-    l_value = t.value.lower()
-    if l_value == "false" or l_value == "true":
-        if t.value[0] != "f" and t.value[0] != 't':
-            return t
-    t.type = reserved.get(l_value, "OBJECTIDENTIFIER")
-    return t
-
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
-
-def t_LINECOMMENT(t):
-    r'--.*\n'
-def t_COMMENT(t):
-    r'\(\*(.|\n)*\*\)'
-
-def t_eof(t):
-    print("HERE")
-    print(t.lexer.lineno)
-    t.lexer.eof =(t.lexer.lineno, compute_column(t))
-    return None
-
-def t_ERROREOFCOMMENTS(t):
-    r'\(\*(.|\n)*$'
-    global __errors__
-    line = t.lexer.lineno
-    column = compute_column(t)
-    __errors__.append(f"({t.lexer.eof[0]},{t.lexer.eof[1]}) - LexicographicError: EOF IN COMMENTS")
+    # def t_comments_TYPEIDENTIFIER(self, t):
+    #     r'[A-Z][a-zA-Z0-9|_]*'
     
-def compute_column(token):
-    line_start = __text__.rfind('\n', 0, token.lexpos) + 1
-    return (token.lexpos - line_start) + 1
+    # def t_comments_OBJECTIDENTIFIER(self, t):
+    #     r'[a-z][a-zA-Z0-9|_]*'
+    
+    # def t_comments_anything(self, t):
+        # r'.*'
+        
+    def t_TYPEIDENTIFIER(self, t):
+        r'[A-Z][a-zA-Z0-9|_]*'
+        l_value = t.value.lower()
+        if l_value == "false" or l_value == "true":
+            if t.value[0] != "f" and t.value[0] != 't':
+                return t
+        t.type = CoolLexer.reserved.get(l_value, "TYPEIDENTIFIER")
+        return t
 
-def t_error(t):
-    global __errors__
-    line = t.lexer.lineno
-    column = compute_column(t)
-    error_text = t.value[0]
-    __errors__.append(f"({line},{column}) - LexicographicError: \"{error_text}\"")
-    t.lexer.skip(1)
+    def t_OBJECTIDENTIFIER(self,t):
+        r'[a-z][a-zA-Z0-9|_]*'
+        l_value = t.value.lower()
+        if l_value == "false" or l_value == "true":
+            if t.value[0] != "f" and t.value[0] != 't':
+                return t
+        t.type = CoolLexer.reserved.get(l_value, "OBJECTIDENTIFIER")
+        return t
 
+    def t_NUMBER(self, t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
 
-def tokenize(text):
-    global __text__
-    global __lexer__
-    global __errors__
-    __errors__ = []
-    __text__ = text
-    if __lexer__ is None:
-        __lexer__ = lex.lex()
-        __lexer__.eof = (1,1)
-    __lexer__.input(text)
-    original_tokens = [token for token in __lexer__]
-    tokens =  [ Token(token.value, tokenType[token.type]) for token in original_tokens]
-    EOF = Token('$', eof)
-    return (tokens + [EOF], __errors__)
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    def t_LINECOMMENT(self, t):
+        r'--.*'
+    
+    def t_COMMENTIN(self, t):
+        r'\(\*'
+        self.lexer.comment_level = 1
+        self.lexer.begin('comments')
+    
+    def t_comments_COMMENTIN(self, t):
+        r'\(\*'
+        self.lexer.comment_level += 1
+            
+    def t_comments_COMMENTOUT(self, t):
+        r'\*\)'
+        if self.lexer.comment_level == 1:
+            self.lexer.begin('INITIAL')
+        self.lexer.comment_level -= 1
+            
+        
+    def t_eof(self, t):
+        t.lexer.eof =(t.lexer.lineno, self.compute_column(t))
+        return None
+
+    def t_comments_eof(self, t):
+        line = t.lexer.lineno
+        column = self.compute_column(t)
+        self.errors.append(f"({line},{column}) - LexicographicError: EOF in comment")
+
+    def compute_column(self, token):
+        line_start = self.text.rfind('\n', 0, token.lexpos) + 1
+        return (token.lexpos - line_start) + 1
+
+    def t_error(self, t):
+        line = t.lexer.lineno
+        column = self.compute_column(t)
+        error_text = t.value[0]
+        self.errors.append(f"({line},{column}) - LexicographicError: \"{error_text}\"")
+        t.lexer.skip(1)
+    
+    def t_comments_error(self, t):
+        t.lexer.skip(1)
+    
+    def tokenize(self, text):
+        self.errors = []
+        self.text = text
+        self.lexer.input(text)
+        original_tokens = [token for token in self.lexer]
+        tokens =  [ Token(token.value, self.tokenType[token.type]) for token in original_tokens]
+        EOF = Token('$', eof)
+        return (tokens + [EOF], self.errors)
--- src/Files_from_TypeInferencer/main.py
+++ src/Files_from_TypeInferencer/main.py
@@ -3,7 +3,9 @@ import logging
 from time import sleep
 from core.cmp.visitors import *
 from core.cmp.evaluation import *
-from core.cmp.lex import tokenize
+from core.cmp.lex import CoolLexer
+import sys
+from os.path import basename
 
 
 def build_AST(G, text):
@@ -111,64 +113,13 @@ def main():
 
 if __name__ == '__main__':
     # main()
-    test_data = '''--Any characters between two dashes “--” and the next newline
---(or EOF, if there is no next newline) are treated as comments
-
-(*(*(*
-Comments may also be written by enclosing
-text in (∗ . . . ∗). The latter form of comment may be nested.
-Comments cannot cross file boundaries.
-*)*)*)
-
-class Error() {
-
-        (* There was once a comment,
-         that was quite long.
-         But, the reader soon discovered that
-         the comment was indeed longer than
-         previously assumed. Now, the reader
-         was in a real dilemma; is the comment
-         ever gonna end? If I stop reading, will
-         it end?
-         He started imagining all sorts of things.
-         He thought about heisenberg's cat and how
-         how that relates to the end of the sentence.
-         He thought to himself "I'm gonna stop reading".
-         "If I keep reading this comment, I'm gonna know
-         the fate of this sentence; That will be disastorous."
-         He knew that such a comment was gonna extend to
-         another file. It was too awesome to be contained in
-         a single file. And he would have kept reading too...
-         if only...
-         cool wasn't a super-duper-fab-awesomest language;
-         but cool is that language;
-         "This comment shall go not cross this file" said cool.
-         Alas! The reader could read no more.
-         There was once a comment,
-         that was quite long.
-         But, the reader soon discovered that
-         the comment was indeed longer than
-         previously assumed. Now, the reader
-         was in a real dilemma; is the comment
-         ever gonna end? If I stop reading, will
-         it end?
-         He started imagining all sorts of things.
-         He thought about heisenberg's cat and how
-         how that relates to the end of the sentence.
-         He thought to himself "I'm gonna stop reading".
-         "If I keep reading this comment, I'm gonna know
-         the fate of this sentence; That will be disastorous."
-         He knew that such a comment was gonna extend to
-         another file. It was too awesome to be contained in
-         a single file. And he would have kept reading too...
-         if only...
-         cool wasn't a super-duper-fab-awesomest language;
-         but cool is that language;
-         "This comment shall go not cross this file" said cool.
-         Alas! The reader could read no more.'''
-    test_data2 = '''(*(*sdfaerlkj
-    asdf*)'''
-    tok, errors = tokenize(test_data)
+    input_file = sys.argv[1]
+    text = ""
+    with open(input_file, 'r') as f:
+        text = f.read()
+    lex = CoolLexer()
+    lex.build()
+    toks, errors = lex.tokenize(text)
+    print(f"TEST {basename(input_file)}")
     print(errors)
-    print(tok)
-   
+       
