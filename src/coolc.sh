4860760ce32a9c14ca85d33a563f533944744866
Marcos Maceo <marcosmaceowork@gmail.com>
2020-02-23 19:45:14 -0500

    [coolc.sh] Update coolc.sh content

--- src/coolc.sh
+++ src/coolc.sh
@@ -4,8 +4,10 @@ INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
-echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"        # TODO: Recuerde cambiar estas
-echo "Copyright (c) 2019: Nombre1, Nombre2, Nombre3"    # TODO: líneas a los valores correctos
+echo "Cool ml-uh v0.0.1"
+echo "Copyright (c) 2019: Lazaro Jesus Suarez Nuñez, Marcos Antonio Maceo Reyes"
 
 # Llamar al compilador
 echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
+
+python compiler.py "$INPUT_FILE" -o "$OUTPUT_FILE"
