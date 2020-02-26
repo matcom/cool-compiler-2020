7c41e2031c91fd97b9138f34fe66edbef744092e
DiazRock <corolariodiaz@gmail.com>
2020-02-26 15:40:47 -0500

    First step in development. Go to other branch

--- src/coolc.sh
+++ src/coolc.sh
@@ -5,7 +5,7 @@ OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
 echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"        # TODO: Recuerde cambiar estas
-echo "Copyright (c) 2019: Nombre1, Nombre2, Nombre3"    # TODO: líneas a los valores correctos
+echo "Copyright (c) 2020: Alejandro Díaz Roque, Rafael Horrach"    # TODO: líneas a los valores correctos
 
 # Llamar al compilador
 echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
