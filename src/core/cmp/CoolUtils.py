19527e306d38bb3ed1e991ad02a41aaa2c88a6c4
AntiD2ta <mtpotro41@gmail.com>
2020-02-26 10:18:33 -0500

    Fix attributes of `factor_3` and `invocation` rules of Grammar

--- src/core/cmp/CoolUtils.py
+++ src/core/cmp/CoolUtils.py
@@ -260,11 +260,11 @@ factor_2 %= factor_3, lambda h, s: s[1]
 # <factor-3>
 factor_3 %= atom, lambda h, s: s[1]
 factor_3 %= atom + func_call, lambda h, s: FunctionCallNode(s[1], *s[2])
-factor_3 %= atom + func_call + invocation, lambda h, s: FunctionCallNode(s[1], *s[2])
+factor_3 %= atom + func_call + invocation, lambda h, s: s[3], None, None, lambda h, s: FunctionCallNode(s[1], *s[2])
 
 # <invocation>
-invocation %= func_call + func_call, lambda h, s: FunctionCallNode(s[1], *s[2])
-invocation %= func_call + invocation, lambda h, s: FunctionCallNode(s[1], *s[2])
+invocation %= func_call, lambda h, s: s[1]
+invocation %= func_call + invocation, lambda h, s: s[2], None, lambda h, s: FunctionCallNode(h[0], *s[1])
 
 # <func-call>
 func_call %= dot + idx + opar + arg_list + cpar, lambda h, s: (s[2], s[4])
