ec24a5a7fcc08881ddbea41147cca7f6299c6b8f
LSilvaO <lisetsilvao2003@gmail.com>
2020-02-26 10:55:32 -0500

    Primera entrega lexer y parser

--- src/coolc.sh
+++ src/coolc.sh
@@ -5,7 +5,8 @@ OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
 echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"        # TODO: Recuerde cambiar estas
-echo "Copyright (c) 2019: Nombre1, Nombre2, Nombre3"    # TODO: líneas a los valores correctos
+echo "Copyright (c) 2019: Liset Silva Oropesa, Pablo A. de Armas Suárez, Yenli Gil Machado"    # TODO: líneas a los valores correctos
 
 # Llamar al compilador
-echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
+#echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
+python COOLCompiler.py INPUT_FILE OUTPUT_FILE
