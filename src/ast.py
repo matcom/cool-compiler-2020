bb9f84d53fddcb33ee76b4d4ccbe4fd090caa80c
Marcos Maceo <marcosmaceowork@gmail.com>
2020-02-26 04:06:59 -0500

    Add basic ast types

---
+++ src/ast.py
@@ -0,0 +1,117 @@
+class AST:
+    def __init__(self):
+        pass
+
+    @property
+    def class_name(self):
+        return str(self.__class__.__name__)
+
+    def to_tuple(self):
+        return tuple([
+            ("class_name", self.class_name)
+        ])
+
+    def to_readable(self):
+        return f"{self.class_name}"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return str(self.to_readable())
+
+
+class Program(AST):
+    def __init__(self, classes):
+        super(Program, self).__init__()
+        self.classes = classes
+
+    def to_tuple(self):
+        return tuple([
+            ("class_name", self.class_name),
+            ("classes", self.classes)
+        ])
+
+    def to_readable(self):
+        return f"{self.class_name}(classes={self.classes})"
+
+
+class Class(AST):
+    def __init__(self, name, parent, features):
+        super(Class, self).__init__()
+        self.name = name
+        self.parent = parent
+        self.features = features
+
+    def to_tuple(self):
+        return tuple([
+            ("class_name", self.class_name),
+            ("name", self.name),
+            ("parent", self.parent),
+            ("features", self.features)
+        ])
+
+    def to_readable(self):
+        return f"{self.class_name}(name='{self.name}', parent={self.parent}, features={self.features})"
+
+
+class ClassFeature(AST):
+    def __init__(self):
+        super(ClassFeature, self).__init__()
+
+
+class ClassMethod(ClassFeature):
+    def __init__(self, name, formal_params, return_type, body):
+        super(ClassMethod, self).__init__()
+        self.name = name
+        self.formal_params = formal_params
+        self.return_type = return_type
+        self.body = body
+
+    def to_tuple(self):
+        return tuple([
+            ("class_name", self.class_name),
+            ("name", self.name),
+            ("formal_params", self.formal_params),
+            ("return_type", self.return_type),
+            ("body", self.body)
+        ])
+
+    def to_readable(self):
+        return f"{self.class_name}(name='{self.name}', formal_params={self.formal_params}, return_type={self.return_type}, body={self.body})"
+
+
+class ClassAttribute(ClassFeature):
+    def __init__(self, name, attr_type, init_expr):
+        super(ClassAttribute, self).__init__()
+        self.name = name
+        self.attr_type = attr_type
+        self.init_expr = init_expr
+
+    def to_tuple(self):
+        return tuple([
+            ("class_name", self.class_name),
+            ("name", self.name),
+            ("attr_type", self.attr_type),
+            ("init_expr", self.init_expr)
+        ])
+
+    def to_readable(self):
+        return f"{self.class_name}(name='{self.name}', attr_type={self.attr_type}, init_expr={self.init_expr})"
+
+
+class FormalParam(ClassFeature):
+    def __init__(self, name, param_type):
+        super(FormalParam, self).__init__()
+        self.name = name
+        self.param_type = param_type
+
+    def to_tuple(self):
+        return tuple([
+            ("class_name", self.class_name),
+            ("name", self.name),
+            ("param_type", self.param_type)
+        ])
+
+    def to_readable(self):
+        return f"{self.class_name}(name='{self.name}', param_type={self.param_type})"
