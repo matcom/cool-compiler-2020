78312734f7666d4ffb6b8908f2d1ab4b3d4e7957
Roberto Marti Cedeño <rmarticedeno@gmail.com>
2020-02-25 23:39:16 -0500

    Added run sentence to coolc.sh

--- src/coolc.sh
+++ src/coolc.sh
@@ -4,8 +4,10 @@ INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
-echo "DLR MATCOM COOL COMPILER V 0.1"        # TODO: Recuerde cambiar estas
-echo "Copyright (c) 2020: Leonel Alejandro García López, Jorge Daniel Valle Díaz, Roberto Marti Cedeño"    # TODO: líneas a los valores correctos
+echo "DLR MATCOM COOL COMPILER V 0.1"
+echo "Copyright (c) 2020: Leonel Alejandro García López, Jorge Daniel Valle Díaz, Roberto Marti Cedeño"
 
 # Llamar al compilador
-echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
+#echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
+
+python3 main.py $INPUT_FILE $OUTPUT_FILE
