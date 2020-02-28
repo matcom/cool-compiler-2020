dc8f0f12f44835db095e61247762ce7bc51026d9
stdevlag <l.garcia3@estudiantes.matcom.uh.cu>
2020-02-28 10:51:23 -0500

    [error] add SyntacticError.

--- src/engine/errors.py
+++ src/engine/errors.py
@@ -3,3 +3,6 @@ ERROR = '(%d, %d) - %s: %s'
 
 def LexicographicError(line, col, message):
     return ERROR % (line, col, "LexicographicError", message)
+
+def SyntacticError(line, col, message):
+    return ERROR % (line, col, "SyntacticError", message)
