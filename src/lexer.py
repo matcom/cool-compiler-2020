02e4559074fc75e889bd4353e3ae51f979167a7f
lazaro <l.suarez@estudiantes.matcom.uh.cu>
2020-02-28 01:18:22 -0500

    [lexer] Update lexer, solve minor bugs

--- src/lexer.py
+++ src/lexer.py
@@ -31,13 +31,15 @@ tokens = [
              'DIVIDE',
              'LPAREN',
              'RPAREN',
-             'ID',
+             'ATTRIBUTEID',
+             'CLASSID',
              'LBRACE',
              'RBRACE',
-             'COLON',
+             'COMMA',
              'SEMICOLON',
-             'DECLARATION',
+             'COLON',
              'ASSIGNATION',
+             'ARROW',
              'DOT',
              'LESS',
              'LESSEQUAL',
@@ -48,35 +50,18 @@ tokens = [
              'DISPATCH'
          ] + list(reserved.values())
 
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LBRACE = r'\{'
-t_RBRACE = r'\}'
-t_COLON = r','
-t_SEMICOLON = r';'
-t_DECLARATION = r':'
-t_ASSIGNATION = r'<-'
-t_DOT = r'\.'
-t_LESS = r'<'
-t_LESSEQUAL = r'<='
-t_GREATEREQUAL = r'>='
-t_GREATER = r'>'
-t_EQUAL = r'='
-t_COMPLEMENT = r'~'
-t_DISPATCH = r'@'
+t_ignore = ' \t\r\v\f'
 
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+def t_INLINECOMMENT(t):
+    r'--.*'
+    pass  
 
-states = (
-    ('STRING','exclusive'),
- )
+def t_start_comment(t):
+    r'\(\*'
+    t.lexer.push_state("COMMENT")
+    t.lexer.counter = 1
+    t.lexer.star = False
+    t.lexer.lparen = False
 
 def t_start_string(t):
     r'"'
@@ -87,6 +72,127 @@ def t_start_string(t):
     t.lexer.string_nullrow = 0
     t.lexer.string_nullcol = 0
 
+def t_PLUS(t):
+    r'\+'
+    t.value = '+'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+
+def t_MINUS(t):
+    r'-'
+    t.value = '-'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+
+def t_TIMES(t):
+    r'\*'
+    t.value = '*'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+
+def t_DIVIDE(t):
+    r'/'
+    t.value = '/'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_LPAREN(t):
+    r'\('
+    t.value = '('
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+
+def t_RPAREN(t):
+    r'\)'
+    t.value = ')'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_LBRACE(t):
+    r'\{'
+    t.value = '{'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_RBRACE(t):
+    r'\}'
+    t.value = '}'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_COMMA(t):
+    r','
+    t.value = ','
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_SEMICOLON(t):
+    r';'
+    t.value = ';'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_COLON(t):
+    r':'
+    t.value = ':'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_ASSIGNATION(t):
+    r'<-'
+    t.value = '<-'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_ARROW(t):
+    r'=>'
+    t.value = '=>'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_DOT(t):
+    r'\.'
+    t.value = '.'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_LESSEQUAL(t):
+    r'<='
+    t.value = '<='
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_LESS(t):
+    r'<'
+    t.value = '<'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_GREATEREQUAL(t):
+    r'>='
+    t.value = '>='
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_GREATER(t):
+    r'>'
+    t.value = '>'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_EQUAL(t):
+    r'='
+    t.value = '='
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_COMPLEMENT(t):
+    r'~'
+    t.value = '~'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+def t_DISPATCH(t):
+    r'@'
+    t.value = '@'
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+
+states = (
+    ('STRING','exclusive'),
+    ('COMMENT', 'exclusive'),
+ )
+
 def t_STRING_newline(t):
     r'\n'
     t.lexer.lineno += 1
@@ -96,10 +202,45 @@ def t_STRING_newline(t):
     else:
         t.lexer.string_backslashed = False
 
+def t_COMMENT_eof(t):
+    r'\$'
+    s = t.lexer.lexdata
+    lineCount = s.count('\n') + 1
+    posCount = 1
+    i = len(s) - 1
+    while i >=0 and s[i] != '\n':
+        posCount += 1
+        i -= 1
+    print("(%s, %s) - LexicographicError: EOF in comment" % (lineCount, find_column(t.lexer.lexdata, t) - 1))
+
+def t_COMMENT_star(t):
+    r'\*'
+    if(t.lexer.lparen):
+        t.lexer.lparen = False
+        t.lexer.counter += 1
+    else:
+        t.lexer.star = True
+
+def t_COMMENT_lparen(t):
+    r'\('
+    t.lexerlparen = True
+    if(t.lexer.star):
+        t.lexer.start = False
+
 def t_STRING_eof(t):
     r'\$'
     print("(%s, %s) - LexicographicError: EOF in string constant" % (t.lineno, find_column(t.lexer.lexdata, t) - 1))
     t.lexer.pop_state()
+
+def t_COMMENT_rparen(t):
+    r'\)'
+    if(t.lexer.star):
+        t.lexer.star = False
+        t.lexer.counter -= 1
+        if(t.lexer.counter == 0):
+            t.lexer.pop_state()
+          
+    
 
 def t_STRING_end(t):
     r'"'
@@ -111,10 +252,15 @@ def t_STRING_end(t):
         else:
             t.value = t.lexer.stringbuf
             t.type = "STRING"
+            t.colno = find_column(t.lexer.lexdata, t)
             return t
     else:
         t.lexer.stringbuf += '"'
         t.lexer.string_backslashed = False
+
+def t_COMMENT_anything(t):
+    r'(.|\n)'
+    pass
 
 def t_STRING_anything(t):
     r'[^\n]'
@@ -147,32 +293,39 @@ def t_STRING_error(t):
     t.lexer.string_nullrow = t.lineno
     t.lexer.string_nullcol = find_column(t.lexer.lexdata, t) - 1
 
-def t_BADCOMMENT(t):
-    r'\(\*(.|\n)*'
-    s = t.lexer.lexdata
-    lineCount = s.count('\n') + 1
-    posCount = 1
-    i = len(s) - 1
-    while i >=0 and s[i] != '\n':
-        posCount += 1
-        i -= 1
-    print("(%s, %s) - LexicographicError: EOF in comment" % (lineCount, posCount - 1))
+t_COMMENT_ignore = ''
+
+def t_COMMENT_error(t):
+    t.lexer.counter = 0
+    t.lexer.star = False
+    t.lexer.lparen = False
 
 def t_newline(t):
      r'\n+'
      t.lexer.lineno += len(t.value)
 
-def t_ID(t):
-    r'[a-zA-Z][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'ID')
+def t_ATTRIBUTEID(t):
+    r'[a-z][a-zA-Z_0-9]*'
+    if reserved.get(t.value.lower()) == None:
+        t.type = 'ATTRIBUTEID'
+    else:
+        t.type = reserved.get(t.value.lower())
+    t.colno = find_column(t.lexer.lexdata, t)
+    return t
+
+def t_CLASSID(t):
+    r'[A-Z][a-zA-Z_0-9]*'
+    if reserved.get(t.value.lower()) == None or reserved.get(t.value.lower()) == 'TRUE' or reserved.get(t.value.lower()) == 'FALSE':
+        t.type = 'CLASSID'
+    else:
+        t.type = reserved.get(t.value.lower())
+    t.colno = find_column(t.lexer.lexdata, t)
     return t
 
 def find_column(input, token):
     line_start = input.rfind('\n', 0, token.lexpos) + 1
     return (token.lexpos - line_start) + 1
 
-t_ignore = ' \t'
-t_ignore_INLINECOMMENT = r'--.*'
 def t_error(t):
     if not(t.value[0] == '$' and t.lexpos + 1 == len(t.lexer.lexdata)):
         print('(%s, %s) - LexicographicError: ERROR "%s"' % (t.lineno, find_column(t.lexer.lexdata, t), t.value[0]))
@@ -206,6 +359,7 @@ def make_lexer(data):
                 continue
         newData += data[i]
         i += 1
+    
     newData = newData + '$'
     lexer = lex.lex()
     lexer.input(newData)
