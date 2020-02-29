888a63b7c3e2a8bbff3fb3d9bd85db7e2e819389
DiazRock <corolariodiaz@gmail.com>
2020-02-29 15:08:18 -0500

    Added functionality for lexer analyzer

--- src/compiler/components/lexer_analyzer.py
+++ src/compiler/components/lexer_analyzer.py
@@ -1,13 +1,22 @@
 from .abstract_component import Component
-
+from ..utils.compiler_containers import lexer_analyzer_dependency_container as injector
+from ply import lex
 
 class lexer_analyzer(Component):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        
 
     @property
-    def tokens_collection():
-        pass
-    
+    def tokens_collection(self):
+        return  injector.tokens_collection_cool
+        
+    @property
+    def basic_reserved(self):
+        return injector.reserved_keywords_cool
+
     def build_component(self):
-        super().build_component()        
+        self.reserved = self.basic_reserved.keys()
+        self.tokens = self.tokens_collection + tuple(self.basic_reserved.values())
+        self.lexer = lex.lex(module = self)
+        
