c93e1c870b5a2215c88ebeb66ccf19b7096dfdd2
TTigre <48805055+TTigre@users.noreply.github.com>
2020-02-29 21:17:27 -0500

    Update coolc.sh
    Quitado acento

--- src/coolc.sh
+++ src/coolc.sh
@@ -5,7 +5,7 @@ OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
 echo "Tiger and Buti Compiler 2020 V0.2.0"        # TODO: Recuerde cambiar estas
-echo "Copyright (c) 2020: José Gabriel Navarro Comabella, Alberto Helguera Fleitas"    # TODO: líneas a los valores correctos
+echo "Copyright (c) 2020: Jose Gabriel Navarro Comabella, Alberto Helguera Fleitas"    # TODO: líneas a los valores correctos
 
 # Llamar al compilador
 echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
