a5b8569903bfc879c31bf2770746aec7b81e9c8e
DiazRock <corolariodiaz@gmail.com>
2020-02-29 20:46:39 -0500

    The moment for test is near. I can feel it.

--- src/compiler/components/lexer_analyzer.py
+++ src/compiler/components/lexer_analyzer.py
@@ -8,22 +8,25 @@ class lexer_analyzer(Component):
                 basic_keywords,
                 simple_rules,
                 complex_rules,
+                error_handlers,
                 *args,
                 **kwargs):                
-        self.inject_complex_rules(complex_rules)
+        self.inject(complex_rules)
+        self.inject(error_handlers)
         self.tokens_collection = tokens_collection
         self.basic_keywords = basic_keywords
         super().__init__(*args, **kwargs)
 
-    def inject_complex_rules(self, complex_rules):
-        for x in complex_rules:
-            super().__setattr__(x.__name__, x)
+    def inject(self, function_group):
+        for function in function_group:
+            super().__setattr__(function.__name__, function)
 
     
     def build_component(self):
         self.reserved = self.basic_keywords.keys()
         self.tokens = self.tokens_collection + tuple(self.basic_keywords.values())
         self.lexer = lex.lex(module = self)
+
         
     def input_lexer(self, cool_program_source_code):
         self.lexer.input(cool_program_source_code)
@@ -32,3 +35,37 @@ class lexer_analyzer(Component):
     def token(self):
         self.last_token = self.lexer.token()
         return self.last_token
+
+
+    # A funny iterator here
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        t = self.token()
+        if t is None:
+            raise StopIteration
+        return t
+
+    def next(self):
+        return self.__next__()
+    # End of fun
+
+
+
+if __name__ == "__main__":
+    import sys
+
+    input_info = sys.argv[1]
+    with open(input_info, encoding = 'utf-8') as file:
+        cool_program_source_code = file.read()
+    
+    lexer = lexer_analyzer(tokens_collection = injector.tokens_collection_cool,
+                        basic_keywords = injector.reserved_keywords_cool, 
+                        simple_rules = injector.simple_rules_cool, 
+                        complex_rules = injector.complex_rules_cool,
+                        error_handlers = injector.error_handlers_cool)
+    lexer.input_lexer(cool_program_source_code)
+
+    for token in lexer:
+        print(token)
--- src/main.py
+++ src/main.py
@@ -4,9 +4,11 @@ from compiler.utils.compiler_containers import container_dict
 
 parser_input =  ArgumentParser(description= 'This is the Diaz-Horrach cool compiler, an school project.\nRead this help and see the ofitial repo')
 parser_input.add_argument('files_for_compile', help = 'The file(s) to be compiled', nargs= '+')
-parser_input.add_argument("--lexer", help = 'Select the lexer that you could use from avialable options', choices= container_dict['lexer_options'].keys())
-parser_input.add_argument("--parser", help = 'Select the lexer that you could use from avialable options', choices= container_dict['parser_options'].keys())
-parser_input.add_argument("--output", help = 'Put the info of the selected components in the standard output.', choices= ['l','p','t'])
+parser_input.add_argument("--lexer", help = 'Select the lexer that you could use from avialable options', choices = container_dict['lexer_options'].keys())
+parser_input.add_argument("--parser", help = 'Select the lexer that you could use from avialable options', choices = container_dict['parser_options'].keys())
+parser_input.add_argument("--output", help = 'Put the info of the selected components in the standard output.', choices = ['l','p','t'])
+
 args = parser_input.parse_args()
 working_input = fileinput.input(files = args.files_for_compile)
+
 
