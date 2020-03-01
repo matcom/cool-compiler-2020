328a1f9463d68a23b4a100ddbbf57244687419a6
Pablo Antonio de Armas Suárez <paas0725@gmail.com>
2020-03-01 03:09:50 -0500

    Se arreglaron varios errores Se modificaron el lexer y el parser para que sean compatibles con lo que esperan los test automáticos Se mejoró el código para soportar PyTest en Windows

--- src/COOLLexerErrorListener.py
+++ src/COOLLexerErrorListener.py
@@ -4,4 +4,4 @@ from antlr4.error.ErrorListener import ErrorListener
 
 class COOLLexerErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-        print("(" + str(line) + ", " + str(column+1) + ") - LexicographicError: " + msg, file=sys.stderr)
+        print("(" + str(line) + ", " + str(column+1) + ") - LexicographicError: " + msg, file=sys.stdout)
