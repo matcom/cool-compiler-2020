b584b040949107e714dad828d77c3c919032b51c
pablodearmas <57228410+pablodearmas@users.noreply.github.com>
2020-03-01 00:17:01 -0500

    Se actualizó nombre y versión del compilador

--- src/coolc.sh
+++ src/coolc.sh
@@ -4,8 +4,8 @@ INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
-echo "LINEA_CON_NOMBRE_Y_VERSION_DEL_COMPILADOR"        # TODO: Recuerde cambiar estas
-echo "Copyright (c) 2019: Liset Silva Oropesa, Pablo A. de Armas Suárez, Yenli Gil Machado"    # TODO: líneas a los valores correctos
+echo "COOLCompiler 1.0.1"
+echo "Copyright (c) 2019: Liset Silva Oropesa, Pablo A. de Armas Suárez, Yenli Gil Machado"
 
 # Llamar al compilador
 #echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
