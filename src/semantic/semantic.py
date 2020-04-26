f2e7dc16549d671bef289fbdd2631298ee4007e5
stdevAdrianPaez <adriantpaez@gmail.com>
2020-04-25 22:07:53 -0400

    Add types structure

--- src/semantic/semantic.py
+++ src/semantic/semantic.py
@@ -4,9 +4,7 @@ from .types import *
 
 
 class Visitor:
-    def __init__(self, method_env, object_env, current_class):
-        self.MethodEnv = method_env
-        self.ObjectEnv = object_env
+    def __init__(self, current_class):
         self.CurrentClass = current_class
 
     def visit(self, node):
@@ -15,113 +13,168 @@ class Visitor:
 
 def __check_type_declaration__(node: ProgramNode):
     for c in node.classes:
-        if c.type in Types:
+        try:
+            _ = TypesByName[c.type]
             add_semantic_error(0, 0, f'duplicated declaration of type {c.type}')
             return False
-        else:
-            Types.append(c.type)
+        except KeyError:
+            TypesByName[c.type] = CoolType(c.type, None)
     return True
 
 
 def __check_type_hierarchy__(node: ProgramNode):
     for c in node.classes:
+        cType = TypesByName[c.type]
         if c.parent_type:
-            if c.parent_type in Types:
-                TypesHierarchy[c.type] = c.parent_type
-            else:
+            try:
+                cType.parent = TypesByName[c.parent_type]
+            except KeyError:
                 add_semantic_error(0, 0, f'unknown parent type {c.parent_type}')
                 return False
         else:
-            TypesHierarchy[c.type] = ObjectType
+            cType.parent = ObjectType
     return True
 
 
-def __check_inherits__(typeA, typeB):
-    '''
-    Return True if typeA <= typeB
-    '''
-    current = typeA
-    while current != typeB:
+def __check_inherits__(type_a: CoolType, type_b: CoolType):
+    """
+    Return True if type_a <= typeB
+    """
+    current = type_a
+    while current != type_b:
         if current is None:
             return False
-        current = TypesHierarchy[current]
+        current = current.parent
     return True
 
 
 class ProgramVisitor(Visitor):
     def __init__(self):
-        super().__init__({}, {}, None)
+        super().__init__(None)
 
     def visit(self, node: ProgramNode):
         if not (__check_type_declaration__(node) and __check_type_hierarchy__(node)):
             return
+        # Check Main class exists
+        try:
+            TypesByName['Main']
+        except KeyError:
+            add_semantic_error(0, 0, f'Main class undeclared')
+            return
         # Initialize MethodEnv
         for c in node.classes:
-            className = c.type
+            classType = TypesByName[c.type]
             for f in c.feature_nodes:
                 if type(f) is DefFuncNode:
-                    functionName = f.id
-                    functionArgsType = [a[1] for a in f.params]
-                    self.MethodEnv[className, functionName] = functionArgsType
+                    # Add all functions to types
+                    functionArgsTypeNames = [a[1] for a in f.params]
+                    functionArgsType = []
+                    for type_name in functionArgsTypeNames:
+                        # Check all argument types
+                        try:
+                            functionArgsType.append(TypesByName[type_name])
+                        except KeyError:
+                            add_semantic_error(0, 0, f'unknown type {type_name}')
+                            return
+                    # Check returned type
+                    try:
+                        returnType = TypesByName[f.return_type]
+                    except KeyError:
+                        add_semantic_error(0, 0, f'unknown type {f.return_type}')
+                        return
+                    try:
+                        _ = classType.methods[f.id]
+                        # Duplicated method declaration in same class, not overload allowed
+                        add_semantic_error(0, 0, f'method {f.id} already declared in class {classType.name}')
+                    except KeyError:
+                        classType.methods[f.id] = CoolTypeMethod(f.id, functionArgsType, returnType)
+                elif type(f) is DefAttrNode:
+                    # Add all attributes to types
+                    try:
+                        _ = classType.attributes[f.id]
+                        add_semantic_error(0, 0, f'attribute {f.id} already declared')
+                    except KeyError:
+                        try:
+                            attrType = TypesByName[f.type]
+                        except KeyError:
+                            add_semantic_error(0, 0, f'unknown type {f.type}')
+                            return
+                        classType.attributes[f.id] = CoolTypeAttribute(f.id, attrType)
 
         # Visit each class inside
         for c in node.classes:
-            c.accept(DefClassVisitor(self.MethodEnv))
+            c.accept(DefClassVisitor())
 
 
 class DefClassVisitor(Visitor):
-    def __init__(self, method_env):
-        super().__init__(method_env, {}, None)
+    def __init__(self):
+        super().__init__(None)
 
     def visit(self, node: DefClassNode):
-        # Set current class name
-        self.CurrentClass = node.type
-        # Initialize ObjectEnv
-        for feature in node.feature_nodes:
-            if type(feature) is DefAttrNode:
-                self.ObjectEnv[feature.id] = feature.type
+        self.CurrentClass = TypesByName[node.type]
         # Check all features
         for feature in node.feature_nodes:
             if type(feature) is DefAttrNode:
-                feature.accept(DefAttrVisitor(self.MethodEnv, self.ObjectEnv, self.CurrentClass))
+                feature.accept(DefAttrVisitor(self.CurrentClass))
             if type(feature) is DefFuncNode:
-                feature.accept(DefFuncVisitor(self.MethodEnv, self.ObjectEnv, self.CurrentClass))
+                feature.accept(DefFuncVisitor(self.CurrentClass))
 
 
 class DefAttrVisitor(Visitor):
-    def __init__(self, method_env, object_env, current_class):
-        super().__init__(method_env, object_env, current_class)
+    def __init__(self, current_class):
+        super().__init__(current_class)
 
     def visit(self, node: DefAttrNode):
         if node.expr:
-            expr_type = node.expr.accept(DefExpressionVisitor(self.MethodEnv, self.ObjectEnv, self.CurrentClass))
-            if not __check_inherits__(expr_type, self.ObjectEnv[node.id]):
+            expr_type = node.expr.accept(DefExpressionVisitor(self.CurrentClass))
+            attr_type = self.CurrentClass.attributes[node.id].attrType
+            if not __check_inherits__(expr_type, attr_type):
                 add_semantic_error(0, 0, f'Invalid type {expr_type}')
+            else:
+                return attr_type
 
 
 class DefFuncVisitor(Visitor):
-    def __init__(self, method_env, object_env, current_class):
-        super().__init__(method_env, object_env, current_class)
+    def __init__(self, current_class):
+        super().__init__(current_class)
 
     def visit(self, node: DefFuncNode):
-        pass
+        body_type = node.expressions.accept(DefExpressionVisitor(self.CurrentClass))
+        return_type = TypesByName[node.return_type]
+        if __check_inherits__(body_type, return_type):
+            return return_type
+        elif body_type is not None:
+            add_semantic_error(0, 0, f'invalid returned type {body_type}')
 
 
 class DefExpressionVisitor(Visitor):
-    def __init__(self, method_env, object_env, current_class):
-        super().__init__(method_env, object_env, current_class)
+    def __init__(self, current_class):
+        super().__init__(current_class)
 
     def visit(self, node):
         if type(node) is IntNode:
             return IntType
         if type(node) is StringNode:
             return StringType
+        if type(node) is BoolNode:
+            return BoolType
         if type(node) is VarNode:
             try:
-                return self.ObjectEnv[node.id]
+                return self.CurrentClass.attributes[node.id].attrType
             except KeyError:
                 add_semantic_error(0, 0, f'invalid variable {node.id}')
-                return
+                return None
+        if type(node) is AssignNode:
+            try:
+                varType = self.CurrentClass.attributes[node.id].attrType
+            except KeyError:
+                add_semantic_error(0, 0, f'invalid variable {node.id}')
+                return None
+            expressionType = node.accept(DefExpressionVisitor(self.CurrentClass))
+            if __check_inherits__(expressionType, varType):
+                return varType
+            else:
+                return None
 
 
 def semantic_check(node):
--- src/semantic/types.py
+++ src/semantic/types.py
@@ -1,15 +1,40 @@
-ObjectType = 'Object'
-IOType = 'IO'
-IntType = 'Int'
-StringType = 'String'
-BoolType = 'Bool'
+class CoolType:
+    def __init__(self, name, parent_type):
+        self.name = name
+        self.parent = parent_type
+        self.attributes = {}
+        self.methods = {}
 
-Types = [ObjectType, IOType, IntType, StringType, BoolType]
+    def __repr__(self):
+        return f'{self.name}'
 
-TypesHierarchy = {
-    ObjectType: None,
-    IOType: ObjectType,
-    IntType: ObjectType,
-    StringType: ObjectType,
-    BoolType: ObjectType
+    def __str__(self):
+        return f'{self.name}'
+
+
+class CoolTypeAttribute:
+    def __init__(self, id, attr_type):
+        self.id = id
+        self.attrType = attr_type
+
+
+class CoolTypeMethod:
+    def __init__(self, id, args, returned_type):
+        self.returnedType = returned_type
+        self.id = id
+        self.args = args
+
+
+ObjectType = CoolType('Object', None)
+IOType = CoolType('IO', ObjectType)
+IntType = CoolType('Int', ObjectType)
+StringType = CoolType('String', ObjectType)
+BoolType = CoolType('Bool', ObjectType)
+
+TypesByName = {
+    'Object': ObjectType,
+    'IO': IOType,
+    'Int': IntType,
+    'String': StringType,
+    'Bool': BoolType
 }
