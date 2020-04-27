cb7456448fbf4b56cef61289edc37b85be0d13c4
AntiD2ta <mtpotro41@gmail.com>
2020-04-27 17:06:13 -0400

    Auto stash before merge of "grammar_fix" and "origin/TypeChecker"

--- src/core/cmp/CoolUtils.py
+++ src/core/cmp/CoolUtils.py
@@ -401,4 +401,18 @@ class Main {
     } ;
 } ;
 '''
+_text2 = '''
+class Main inherits IO {
+     main() : String {
+         foo(42)
+     };
 
+     foo(i : Int) : String {
+        if i = 0 then "" else 
+	    (let next : Int <- i / 10 in
+		foo(next).concat(foo(i - next * 10))
+	    )
+        fi
+    };
+};
+'''
