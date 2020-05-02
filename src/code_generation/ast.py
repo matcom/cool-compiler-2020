e94b9d0737e4f6a343c647e07af1d4a140d23692
stdevAdrianPaez <adriantpaez@gmail.com>
2020-05-02 15:32:45 -0400

    Reformat code and remove unnecessary imports

--- src/code_generation/ast.py
+++ src/code_generation/ast.py
@@ -1,172 +1,199 @@
 class Node:
     pass
 
+
 class ProgramNode(Node):
     def __init__(self, types, data, code):
         self.types = types
         self.data = data
         self.code = code
-        
+
+
 class TypeNode(Node):
     def __init__(self, type):
-        self.attributes=[]
-        self.methods=[]
-        self.type=type
-        
+        self.attributes = []
+        self.methods = []
+        self.type = type
+
+
 class DataNode(Node):
     def __init__(self, id, val):
-        self.id=id
-        self.val=val
-        
+        self.id = id
+        self.val = val
+
+
 class FuncNode(Node):
     def __init__(self, name, params, locals, body):
-        self.name=name
-        self.params=params
-        self.locals=locals
-        self.body=body
-        
+        self.name = name
+        self.params = params
+        self.locals = locals
+        self.body = body
+
+
 class InstructionNode(Node):
     pass
-        
+
+
 class LocalNode(Node):
     def __init__(self, id):
-        self.id=id
-        
+        self.id = id
+
+
 class ParamNode(Node):
     def __init__(self, id):
-        self.id=id
-        
+        self.id = id
+
+
 class AssignNode(InstructionNode):
     def __init__(self, id, val):
-        self.id=id
-        self.val=val
-        
+        self.id = id
+        self.val = val
+
+
 class ArithNode(InstructionNode):
     def __init__(self, left, right, result):
-        self.left=left
-        self.right=right
-        self.result=result
-        
+        self.left = left
+        self.right = right
+        self.result = result
+
+
 class PlusNode(ArithNode):
     pass
+
 
 class MinusNode(ArithNode):
     pass
 
+
 class StarNode(ArithNode):
     pass
+
 
 class DivNode(ArithNode):
     pass
 
+
 class GetAttrNode(InstructionNode):
     def __init__(self, obj, attr, result):
-        self.attr=attr
-        self.obj=obj
-        self.result=result
-        
+        self.attr = attr
+        self.obj = obj
+        self.result = result
+
+
 class SetAttrNode(InstructionNode):
-    def __init__(self,  obj, attr, val):
-        self.val=val
-        self.obj=obj
-        self.attr=attr
-        
+    def __init__(self, obj, attr, val):
+        self.val = val
+        self.obj = obj
+        self.attr = attr
+
+
 class SetIndexNode(InstructionNode):
     def __init__(self, array, index, val):
-        self.val=val
-        self.array=array
-        self.index=index
-        
+        self.val = val
+        self.array = array
+        self.index = index
+
+
 class GetIndexNode(InstructionNode):
     def __init__(self, array, index, result):
-        self.result=result
-        self.array=array
-        self.index=index
-        
+        self.result = result
+        self.array = array
+        self.index = index
+
+
 class AllocateNode(InstructionNode):
     def __init__(self, type, addr):
-        self.type=type
-        self.addr=addr
-        
+        self.type = type
+        self.addr = addr
+
+
 class TypeOfNode(InstructionNode):
     def __init__(self, result, var):
-        self.result=result
-        self.var=var
-        
+        self.result = result
+        self.var = var
+
+
 class ArrayNode(InstructionNode):
     def __init__(self, len, result):
-        self.len=len
-        self.result=result
-        
+        self.len = len
+        self.result = result
+
+
 class CallNode(InstructionNode):
     def __init__(self, method, result):
-        self.method=method
-        self.result=result
-        
+        self.method = method
+        self.result = result
+
+
 class VCAllNode(InstructionNode):
     def __init__(self, type, method, result):
-        self.method=method
-        self.type=type
-        self.result=result
-        
+        self.method = method
+        self.type = type
+        self.result = result
+
+
 class ArgNode(InstructionNode):
     def __init__(self, val):
-        self.val=val
-        
+        self.val = val
+
+
 class ConditionalGotoNode(InstructionNode):
     def __init__(self, predicate, label):
-        self.predicate=predicate
-        self.label=label
-        
+        self.predicate = predicate
+        self.label = label
+
+
 class GotoNode(InstructionNode):
     def __init__(self, label):
-        self.label=label
-        
+        self.label = label
+
+
 class LabelNode(InstructionNode):
     def __init__(self, label_name):
-        self.label_name=label_name
-        
+        self.label_name = label_name
+
+
 class ReturnNode(InstructionNode):
     def __init__(self, ret_value):
-        self.ret_value=ret_value
+        self.ret_value = ret_value
 
-        
+
 class LoadNode(InstructionNode):
     def __init__(self, addr, result):
-        self.result=result
-        self.addr=addr
-               
+        self.result = result
+        self.addr = addr
+
+
 class LengthNode(InstructionNode):
     def __init__(self, str, result):
-        self.result=result
-        self.str=str
+        self.result = result
+        self.str = str
+
 
 class ConcatNode(InstructionNode):
     def __init__(self, str_a, str_b, result):
-        self.result=result
-        self.str_a=str_a
-        self.str_b=str_b
+        self.result = result
+        self.str_a = str_a
+        self.str_b = str_b
+
 
 class SubStringNode(InstructionNode):
     def __init__(self, str_a, str_b, result):
-        self.result=result
-        self.str_a=str_a
-        self.str_b=str_b
-        
+        self.result = result
+        self.str_a = str_a
+        self.str_b = str_b
+
+
 class StrNode(InstructionNode):
     def __init__(self, val, str):
-        self.str=str
-        self.val=val
-    
-    
+        self.str = str
+        self.val = val
+
+
 class ReadNode(InstructionNode):
     pass
 
+
 class PrintNode(InstructionNode):
     def __init__(self, str):
-        self.str=str
-        
-        
-
-        
-            
+        self.str = str
--- src/code_generation/cil.py
+++ src/code_generation/cil.py
@@ -1,159 +1,160 @@
 import lexer_parser.ast as lp_ast
 
-
-from types import TypesByName
 import code_generation.ast as cil
 
+
 def program_to_cil_visitor(program):
-    types=[]
-    data=[]
-    code=[]
-    #build program node with each section
+    types = []
+    data = []
+    code = []
+    # build program node with each section
     return cil.ProgramNode(types, data, code)
-                
-def func_to_cil_visitor(type_name, func):    
-    name=f'{type_name}_{func.id}'
-    params=[cil.ParamNode(id) for (id, t) in func.params]
-    locals=[]
-    body=[]
-    locals_count=0
+
+
+def func_to_cil_visitor(type_name, func):
+    name = f'{type_name}_{func.id}'
+    params = [cil.ParamNode(id) for (id, t) in func.params]
+    locals = []
+    body = []
+    locals_count = 0
     for exp in func.expressions:
-        instruction=expression_to_cil_visitor(exp, locals_count)
+        instruction = expression_to_cil_visitor(exp, locals_count)
         locals.append(instruction.locals)
         body.append(instruction.body)
-        locals_count+=len(instruction.locals)
-        
+        locals_count += len(instruction.locals)
+
     return cil.FuncNode(name, params, locals, body)
-              
+
+
 def expression_to_cil_visitor(expression, locals_count):
-    #choose appropriate visitor 
+    # choose appropriate visitor
     pass
 
+
 def assign_to_cil_visitor(assign, locals_count):
-    expr=expression_to_cil_visitor(assign.expr, locals_count)
-    locals_count+=len(expr.locals)
-    value=[f'local_{locals_count}']
-    locals=expr.locals+[cil.LocalNode(value)]
-    body=expr.body+[cil.AssignNode(assign.id, expr.value)]
+    expr = expression_to_cil_visitor(assign.expr, locals_count)
+    locals_count += len(expr.locals)
+    value = [f'local_{locals_count}']
+    locals = expr.locals + [cil.LocalNode(value)]
+    body = expr.body + [cil.AssignNode(assign.id, expr.value)]
     return CIL_block(locals, body, value)
+
 
 def arith_to_cil_visitor(arith, locals_count):
-    
-    l=expression_to_cil_visitor(arith.lvalue, locals_count)
-    r=expression_to_cil_visitor(arith.rvalue, locals_count)
-    locals_count+=len(l.locals)+len(r.locals)
-    
-    cil_result=f'local_{locals_count}'
-    
-    locals=l.locals+r.locals+[cil.LocalNode(cil_result)]
-    body=l.body+r.body
-    
-    if type(arith)==lp_ast.PlusNode:
+    l = expression_to_cil_visitor(arith.lvalue, locals_count)
+    r = expression_to_cil_visitor(arith.rvalue, locals_count)
+    locals_count += len(l.locals) + len(r.locals)
+
+    cil_result = f'local_{locals_count}'
+
+    locals = l.locals + r.locals + [cil.LocalNode(cil_result)]
+    body = l.body + r.body
+
+    if type(arith) == lp_ast.PlusNode:
         body.append(cil.PlusNode(l.value, r.value, cil_result))
-    elif type(arith)==lp_ast.MinusNode:
+    elif type(arith) == lp_ast.MinusNode:
         body.append(cil.MinusNode(l.value, r.value, cil_result))
-    elif type(arith)==lp_ast.StarNode:
+    elif type(arith) == lp_ast.StarNode:
         body.append(cil.StarNode(l.value, r.value, cil_result))
-    elif type(arith)==lp_ast.DivNode:
+    elif type(arith) == lp_ast.DivNode:
         body.append(cil.DivNode(l.value, r.value, cil_result))
-      
+
     return CIL_block(locals, body, cil_result)
-    
+
+
 def if_to_cil_visitor(_if, locals_count):
-    predicate=expression_to_cil_visitor(_if.if_expr, locals_count)
-    locals_count+=len(predicate.locals)
-    
-    then=expression_to_cil_visitor(_if.then_expr, locals_count)
-    locals_count+=len(then.locals)
-    
-    else_expression=expression_to_cil_visitor(_if.else_expr, locals_count)
-    locals_count+=len(else_expression.locals)
-        
-    label_1=f'local_{locals_count}'
-    label_2=f'local_{locals_count+1}'
-    value=f'local_{locals_count+2}'
+    predicate = expression_to_cil_visitor(_if.if_expr, locals_count)
+    locals_count += len(predicate.locals)
 
-    locals=predicate.locals+then.locals+else_expression.locals+[cil.LocalNode(value)]
-    body=[cil.ConditionalGotoNode(predicate.value, label_1)]+else_expression.body+[cil.AssignNode(value, else_expression.value),cil.GotoNode(label_2),cil.LabelNode(label_1)]+then.body+[cil.AssignNode(value, then.value),cil.LabelNode(label_2)]
-    
+    then = expression_to_cil_visitor(_if.then_expr, locals_count)
+    locals_count += len(then.locals)
+
+    else_expression = expression_to_cil_visitor(_if.else_expr, locals_count)
+    locals_count += len(else_expression.locals)
+
+    label_1 = f'local_{locals_count}'
+    label_2 = f'local_{locals_count + 1}'
+    value = f'local_{locals_count + 2}'
+
+    locals = predicate.locals + then.locals + else_expression.locals + [cil.LocalNode(value)]
+    body = [cil.ConditionalGotoNode(predicate.value, label_1)] + else_expression.body + [
+        cil.AssignNode(value, else_expression.value), cil.GotoNode(label_2), cil.LabelNode(label_1)] + then.body + [
+               cil.AssignNode(value, then.value), cil.LabelNode(label_2)]
+
     return CIL_block(locals, body, value)
-    
+
+
 def loop_to_cil_visitor(loop, locals_count):
-    predicate=expression_to_cil_visitor(loop.cond, locals_count)
-    locals_count+=len(predicate.locals)
-    
-    loop_block=expression_to_cil_visitor(loop.body, locals_count)
-    locals_count+=len(loop_block.locals)
-    
-    value=f'local_{locals_count}'
+    predicate = expression_to_cil_visitor(loop.cond, locals_count)
+    locals_count += len(predicate.locals)
 
-    locals=predicate.locals+loop_block.locals+[cil.LocalNode(value)]
-    
-    loop_label=f'local_{locals_count+1}'
-    end_label=f'local_{locals_count+2}'
-    
-    body=[cil.ConditionalGotoNode(predicate.value, loop_label), cil.GotoNode(end_label), cil.LabelNode(loop_label)]+loop_block.body+[cil.AssignNode(value, loop_block.value) ,cil.LabelNode(end_label)]
-    
+    loop_block = expression_to_cil_visitor(loop.body, locals_count)
+    locals_count += len(loop_block.locals)
+
+    value = f'local_{locals_count}'
+
+    locals = predicate.locals + loop_block.locals + [cil.LocalNode(value)]
+
+    loop_label = f'local_{locals_count + 1}'
+    end_label = f'local_{locals_count + 2}'
+
+    body = [cil.ConditionalGotoNode(predicate.value, loop_label), cil.GotoNode(end_label),
+            cil.LabelNode(loop_label)] + loop_block.body + [cil.AssignNode(value, loop_block.value),
+                                                            cil.LabelNode(end_label)]
+
     return CIL_block(locals, body, value)
+
 
 def equal_to_cil_visitor(less_than, locals_count):
-    l=expression_to_cil_visitor(arith.lvalue, locals_count)
-    r=expression_to_cil_visitor(arith.rvalue, locals_count)
-    locals_count+=len(l.locals)+len(r.locals)
-    
-    cil_result=f'local_{locals_count}'
-    end_label=f'local_{locals_count+1}'
-    value=f'local_{locals_count+2}'
-    
-    locals=l.locals+r.locals+[cil.LocalNode(cil_result), cil.LocalNode(value)]
-    body=l.body+r.body+[cil.MinusNode(l.value, r.value, cil_result), cil.AssignNode(value, 0),cil.ConditionalGotoNode(cil_result, end_label), cil.AssignNode(value, 1), cil.LabelNode(end_label)]
-    
+    l = expression_to_cil_visitor(arith.lvalue, locals_count)
+    r = expression_to_cil_visitor(arith.rvalue, locals_count)
+    locals_count += len(l.locals) + len(r.locals)
+
+    cil_result = f'local_{locals_count}'
+    end_label = f'local_{locals_count + 1}'
+    value = f'local_{locals_count + 2}'
+
+    locals = l.locals + r.locals + [cil.LocalNode(cil_result), cil.LocalNode(value)]
+    body = l.body + r.body + [cil.MinusNode(l.value, r.value, cil_result), cil.AssignNode(value, 0),
+                              cil.ConditionalGotoNode(cil_result, end_label), cil.AssignNode(value, 1),
+                              cil.LabelNode(end_label)]
+
     return CIL_block(locals, body, value)
-    
-    
+
+
 def integer_to_cil_visitor(integer, locals_count):
     return CIL_block([], [], integer.value)
 
+
 def bool_to_cil_visitor(bool, locals_count):
-    return CIL_block([], [], 1) if bool=='true' else CIL_block([], [], 0)
+    return CIL_block([], [], 1) if bool == 'true' else CIL_block([], [], 0)
+
 
 def id_to_cil_visitor(id, locals_count):
     return CIL_block([], [], id.id)
 
+
 def new_to_cil_visitor(new_node, locals_count):
-    value=f'local_{locals_count}'
-    body=[cil.AllocateNode(new_node.type, value)]
-    locals=[cil.LocalNode(value)]
+    value = f'local_{locals_count}'
+    body = [cil.AllocateNode(new_node.type, value)]
+    locals = [cil.LocalNode(value)]
     return CIL_block(locals, body, value)
 
+
 def string_to_cil_visitor(str, locals_count):
-    str_addr=f'local_{locals_count}'
-    str_id=f'local_{locals_count+1}'
-    
-    locals=[cil.LocalNode(str_id)]
-    data=[cil.DataNode(str_addr, str.value)]
-    body=[cil.LoadNode(str_addr, str_id)]
-    
+    str_addr = f'local_{locals_count}'
+    str_id = f'local_{locals_count + 1}'
+
+    locals = [cil.LocalNode(str_id)]
+    data = [cil.DataNode(str_addr, str.value)]
+    body = [cil.LoadNode(str_addr, str_id)]
+
     return CIL_block(locals, body, str_id, data)
+
 
 class CIL_block:
     def __init__(self, locals, body, value, data=[]):
-        self.locals=locals
-        self.body=body
-        self.value=value
-        self.data=data
-        
-    
-    
-    
-            
-            
-            
-        
-        
-        
-        
-    
-
-
-    
+        self.locals = locals
+        self.body = body
+        self.value = value
+        self.data = data
