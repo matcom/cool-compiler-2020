2f928358ecfb8cd911db9e23d56e80e51e2eb436
stdevAdrianPaez <adriantpaez@gmail.com>
2020-05-04 14:29:49 -0400

    Clean lexer_parser AST

--- src/lexer_parser/ast.py
+++ src/lexer_parser/ast.py
@@ -46,62 +46,61 @@ class DefFuncNode(FeatureNode):
         self.expressions = expressions
 
 
-class AssignNode(AstNode):
+class ExpressionNode(AstNode):
+    def __init__(self):
+        super().__init__()
+        self.returned_type = None
+
+
+class AssignNode(ExpressionNode):
     def __init__(self, id, expr):
         super().__init__()
         self.id = id
         self.expr = expr
-        self.returned_type = None
 
 
-class FuncCallNode(AstNode):
+class FuncCallNode(ExpressionNode):
     def __init__(self, id, args, object=None, type=None):
         super().__init__()
         self.object = object
         self.type = type
         self.id = id
         self.args = args
-        self.returned_type = None
 
 
-class IfNode(AstNode):
+class IfNode(ExpressionNode):
     def __init__(self, if_expr, then_expr, else_expr):
         super().__init__()
         self.if_expr = if_expr
         self.then_expr = then_expr
         self.else_expr = else_expr
-        self.returned_type = None
 
 
-class WhileNode(AstNode):
+class WhileNode(ExpressionNode):
     def __init__(self, cond, body):
         super().__init__()
         self.cond = cond
         self.body = body
-        self.returned_type = None
 
 
-class BlockNode(AstNode):
+class BlockNode(ExpressionNode):
     def __init__(self, expressions):
         super().__init__()
         self.expressions = expressions
-        self.returned_type = None
 
 
-class LetNode(AstNode):
+class LetNode(ExpressionNode):
     def __init__(self, let_attrs, expr):
         super().__init__()
         self.let_attrs = let_attrs
         self.expr = expr
-        self.returned_type = None
 
 
-class CaseNode(AstNode):
+class CaseNode(ExpressionNode):
     def __init__(self, expr, case_list):
         super().__init__()
         self.expr = expr
         self.case_list = case_list
-        self.returned_type = None
 
 
 class CaseElemNode(AstNode):
@@ -110,18 +109,6 @@ class CaseElemNode(AstNode):
         self.expr = expr
         self.id = id
         self.type = type
-
-
-class InitNode(AstNode):
-    def __init__(self, type):
-        super().__init__()
-        self.type = type
-
-
-class ExpressionNode(AstNode):
-    def __init__(self):
-        super().__init__()
-        self.returned_type = None
 
 
 class BinaryNode(ExpressionNode):
@@ -163,7 +150,6 @@ class UnaryNode(ExpressionNode):
     def __init__(self, val):
         super().__init__()
         self.val = val
-        self.returned_type = None
 
 
 class NegationNode(UnaryNode):
@@ -182,25 +168,22 @@ class IsVoidNode(UnaryNode):
     pass
 
 
-class VarNode(AstNode):
+class VarNode(ExpressionNode):
     def __init__(self, id):
         super().__init__()
         self.id = id
-        self.returned_type = None
 
 
-class NewNode(AstNode):
+class NewNode(ExpressionNode):
     def __init__(self, t):
         super().__init__()
         self.type = t
-        self.returned_type = None
 
 
-class ConstantNode(AstNode):
+class ConstantNode(ExpressionNode):
     def __init__(self, value):
         super().__init__()
         self.value = value
-        self.returned_type = None
 
 
 class IntNode(ConstantNode):
