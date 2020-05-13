435d38edb036328542370003fa1db81c9441e404
cbermudez <cbermu.97@gmail.com>
2020-05-13 16:15:23 -0400

    Fix anotation

--- src/cool_lang/ast/let_in_node.py
+++ src/cool_lang/ast/let_in_node.py
@@ -6,5 +6,5 @@ from .let_node import LetNode
 class LetInNode(ExpressionNode):
     def __init__(self, let_body: List[LetNode], in_body: ExpressionNode, line: int, column: int):
         super(LetInNode, self).__init__(line, column)
-        self.let_body: List[LetInNode] = let_body
+        self.let_body: List[LetNode] = let_body
         self.in_body: ExpressionNode = in_body
