78d44442feb6df4d78b635b4dd850f7a5e75d99d
Daniel Enrique <dcordb97@gmail.com>
2020-05-18 15:04:24 -0400

    Changed file reading method.

--- src/main.py
+++ src/main.py
@@ -43,7 +43,7 @@ def main():
     content = ""
 
     with open(args.file_path) as file:
-        content = "".join(file.readlines())
+        content = file.read()
 
     lexical_analysis(content)
     syntactic_analysis(content)
