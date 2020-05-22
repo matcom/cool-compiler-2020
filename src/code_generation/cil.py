76f3bb674ac50f06145ac5ad5f5cc378a5188ea6
stdevAdrianPaez <adriantpaez@gmail.com>
2020-05-22 11:48:02 -0400

    Add class Local

--- src/code_generation/cil.py
+++ src/code_generation/cil.py
@@ -16,11 +16,21 @@ def add_str_data(data: str):
 
 locals_count = 0
 
+class Local:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
 
 def add_local():
     global locals_count
     locals_count += 1
-    return f'local_{locals_count}'
+    return Local(f'local_{locals_count}')
 
 def add_label():
     global labels_count
