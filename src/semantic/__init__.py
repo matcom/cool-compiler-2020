f82dae79dcc68bed49c0a5c922a5725622e68ce8
stdevAdrianPaez <adriantpaez@gmail.com>
2020-05-24 13:36:46 -0400

    Add header to semantic files

--- src/semantic/__init__.py
+++ src/semantic/__init__.py
@@ -1,2 +1,8 @@
+"""
+Copyright (c) 2020 School of Math and Computer Science, University of Havana
+
+COOL compiler project
+"""
+
 from .semantic import semantic_check
 from .types import TypesByName
