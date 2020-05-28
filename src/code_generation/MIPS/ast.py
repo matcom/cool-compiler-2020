1d9aaf7d14abea1ccaf8438d5bb1b9f100a28b89
stdevAdrianPaez <adriantpaez@gmail.com>
2020-05-28 17:08:15 -0400

    Add new MIPS instructions implementations

--- src/code_generation/MIPS/ast.py
+++ src/code_generation/MIPS/ast.py
@@ -1,35 +1,65 @@
+class Instruction:
+    def __init__(self, name, arguments=None):
+        self.name = name
+        self.arguments = arguments
+
+    def __str__(self):
+        if self.arguments is None:
+            return f"{name}"
+        elif len(self.arguments) == 3:
+            return f"{name} {self.arguments[0]}, {self.arguments[1]}, {self.arguments[2]}"
+        elif len(self.arguments) == 2:
+            return f"{name} {self.arguments[0]}, {self.arguments[1]}"
+        elif len(self.arguments) == 1:
+            return f"{name} {self.arguments[0]}"
+
+
+# ===============
+# Data Directives
+# ===============
+
+class AsciizInst(Instruction):
+    def __init__(self, arguments):
+        super().__init__(".asciiz", arguments)
+
+# OLD AST
+
+
 class Node:
     pass
 
+
 class BinaryNode(Node):
     def __init__(self, des, src1, src2):
-        self.des=des
-        self.src1=src1
-        self.src2=src2
+        self.des = des
+        self.src1 = src1
+        self.src2 = src2
+
 
 class AdduNode(BinaryNode):
     def __str__(self):
         return f'addu {self.des}, {self.src1}, {self.src2}'
-    
-    
+
+
 class MuloNode(BinaryNode):
     def __str__(self):
         return f'mulo {self.des}, {self.src1}, {self.src2}'
-    
+
 
 class DivuNode(BinaryNode):
     def __str__(self):
         return f'divu {self.des}, {self.src1}, {self.src2}'
-    
-    
+
+
 class SubuNode(BinaryNode):
     def __str__(self):
         return f'subu {self.des}, {self.src1}, {self.src2}'
-    
+
+
 class SeqNode(BinaryNode):
     def __str__(self):
         return f'seq {self.des}, {self.src1}, {self.src2}'
-    
+
 
 class SneNode(BinaryNode):
     def __str__(self):
@@ -58,137 +88,154 @@ class SltuNode(BinaryNode):
 
 class BNode(Node):
     def __init__(self, lab):
-        self.lab=lab
-        
+        self.lab = lab
+
     def __str__(self):
         return f'b {self.lab}'
 
 
 class BeqzNode(Node):
-    def __init__(self,src, lab):
-        self.src=src
-        self.lab=lab
-        
+    def __init__(self, src, lab):
+        self.src = src
+        self.lab = lab
+
     def __str__(self):
         return f'beqz {self.src}, {self.lab}'
 
+
 class LabelJumpNode(Node):
     def __init__(self, lab):
-        self.lab=lab
-        
+        self.lab = lab
+
+
 class JNode(LabelJumpNode):
     def __str__(self):
         return f'j {self.lab}'
-        
+
+
 class JrNode(Node):
     def __init__(self, src):
-        self.src=src
-    
+        self.src = src
+
     def __str__(self):
         return f'jr {self.src}'
-    
+
+
 class JalNode(LabelJumpNode):
     def __str__(self):
         return f'jal {self.lab}'
-        
+
 
 class AddressNode(Node):
     pass
 
+
 class ConstAddrNode(AddressNode):
     def __init__(self, const):
-        self.const=const
-        
+        self.const = const
+
     def __str__(self):
         return self.const
-        
+
+
 class RegAddrNode(AddressNode):
     def __init__(self, reg, const=None):
-        self.const=const
-        self.reg=reg 
-    
+        self.const = const
+        self.reg = reg
+
     def __str__(self):
         if self.const:
             return f'{self.const}({self.reg})'
         else:
             return f'({self.reg})'
-        
+
+
 class SymbolAddrNode(AddressNode):
     def __init__(self, symbol, const=None, reg=None):
-        self.symbol=symbol  
-        self.const=const
-        self.reg=reg
-        
+        self.symbol = symbol
+        self.const = const
+        self.reg = reg
+
     def __str__(self):
         if self.const and self.reg:
             return f'{self.symbol} + {self.const}({self.reg})'
         if self.const:
             return f'{self.symbol} + {self.const}'
         return self.symbol
-        
+
+
 class LoadAddrNode(Node):
     def __init__(self, des, addr):
-        self.des=des
-        self.addr=addr
+        self.des = des
+        self.addr = addr
+
 
 class LaNode(LoadAddrNode):
     def __str__(self):
         return f'la {self.des}, {self.addr}'
-    
-        
+
+
 class LbuNode(LoadAddrNode):
     def __str__(self):
         return f'lbu {self.des}, {self.addr}'
+
 
 class LhuNode(LoadAddrNode):
     def __str__(self):
         return f'lhu {self.des}, {self.addr}'
 
+
 class LwNode(LoadAddrNode):
     def __str__(self):
         return f'lw {self.des}, {self.addr}'
+
 
 class Ulhu(LoadAddrNode):
     def __str__(self):
         return f'ulhu {self.des}, {self.addr}'
 
+
 class Ulw(LoadAddrNode):
     def __str__(self):
         return f'ulw {self.des}, {self.addr}'
 
+
 class LoadConstNode(Node):
     def __init__(self, des, const):
-        self.des=des
-        self.const=const
-        
+        self.des = des
+        self.const = const
+
+
 class LuiNode(LoadConstNode):
     def __str__(self):
         return f'lui {self.des}, {self.const}'
+
 
 class LiNode(LoadConstNode):
     def __str__(self):
         return f'li {self.des}, {self.const}'
 
+
 class Move(Node):
     def __init__(self, src, des):
-        self.des=des
-        self.src=src
-        
+        self.des = des
+        self.src = src
+
     def __str__(self):
         return f'move {self.des}, {self.src}'
 
+
 class UnaryNode(Node):
     def __init__(self, des, src):
-        self.des=des
-        self.src=src
-        
+        self.des = des
+        self.src = src
+
+
 class NotNode(UnaryNode):
     def __str__(self):
         return f'la {self.des}, {self.src}'
 
+
 class SyscallNode(Node):
     def __str__(self):
         return 'syscall'
-
-
-       
--- src/code_generation/MIPS/mips.py
+++ src/code_generation/MIPS/mips.py
@@ -1,36 +1,41 @@
 from code_generation.CIL import ast as cil
 from . import ast as mips
 
-__METHOD_MAPPING__={}
-__ADDRS__= {}
+__METHOD_MAPPING__ = {}
+__ADDRS__ = {}
+
 
 def save_caller_registers():
     pass
 
+
 def save_callee_registers():
     pass
+
 
 def restore_caller_registers():
     pass
 
+
 def restore_callee_registers():
     pass
 
+
 def push_stack(src):
     pass
-    
-    
 
-def program_to_mips_visitor(program:cil.ProgramNode):
+
+def program_to_mips_visitor(program: cil.ProgramNode):
     global __METHOD_MAPPING__
-    
-    #filling method mapping
+
+    # filling method mapping
     for t in program.types:
         for m in t.methods:
-            __METHOD_MAPPING__[(t,m)]=t.methods[m]
-            
-    data_section=[f'{d.id}: .asciiz \"{d.val}\"' for d in program.data].join('\n')
-    #continue
-    
-def vcall_to_mips_visitor(vcall:cil.VCAllNode):
-    return save_caller_registers() + [mips.JalNode(__METHOD_MAPPING__[(vcall.type, vcall.method)])] + restore_caller_registers() + [mips.Move('v0',__ADDRS__[vcall.result])]
+            __METHOD_MAPPING__[(t, m)] = t.methods[m]
+
+    data_section = [mips.AsciizInst(d.val) for d in program.data]
+    # continue
+
+
+def vcall_to_mips_visitor(vcall: cil.VCAllNode):
+    return save_caller_registers() + [mips.JalNode(__METHOD_MAPPING__[(vcall.type, vcall.method)])] + restore_caller_registers() + [mips.Move('v0', __ADDRS__[vcall.result])]
