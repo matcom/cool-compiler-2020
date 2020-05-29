d928acae1e63e50923502f7fa99820404494369b
Eric <e.martin@estudiantes.matcom.uh.cu>
2020-05-28 20:10:18 -0400

    Add arithmetic operation to MIPS

--- src/code_generation/MIPS/ast.py
+++ src/code_generation/MIPS/ast.py
@@ -55,10 +55,66 @@ class LwInstruction(Instruction):
 # Arithmetic Instructions
 # =======================
         
-class SubuInstruction(Instruction):
-    def __init__(self, arguments):
-        super().__init__('subu', arguments)
+class SubInstruction(Instruction):
+    def __init__(self, arguments, signed = True):
+        super().__init__('sub', arguments) if signed else super().__init__('subu', arguments)
         
-class AdduInstruction(Instruction):
+class AddInstruction(Instruction):
+    def __init__(self, arguments, signed = True):
+        super().__init__('add', arguments) if signed else super().__init__('addu', arguments)
+
+class MultInstruction(Instruction):
+    def __init__(self, arguments, signed = True):
+        super().__init__('mult', arguments) if signed else super().__init__('multu', arguments)
+
+class DivInstruction(Instruction):
+     def __init__(self, arguments, signed = True):
+        super().__init__('div', arguments) if signed else super().__init__('divu', arguments)
+
+class NegInstruction(Instruction):
+    def __init__(self, arguments, signed = True):
+        super().__init__('neg', arguments) if signed else super().__init__('negu', arguments)
+
+class NorInstruction(Instruction):
     def __init__(self, arguments):
-        super().__init__('addu', arguments)
+        super().__init__('nor', arguments)
+
+class NotInstruction(Instruction):
+    def __init__(self, arguments):
+        super().__init__('not', arguments)
+
+class OrInstruction(Instruction):
+    def __init__(self, arguments):
+        super().__init__('or', arguments)
+
+class RemInstruction(Instruction):
+    def __init__(self, arguments, signed = True):
+        super().__init__('rem', arguments) if signed else super().__init__('remu', arguments)
+
+class RolInstruction(Instruction):
+    def __init__(self, arguments):
+        super().__init__('rol', arguments)
+
+class RorInstruction(Instruction):
+    def __init__(self, arguments):
+        super().__init__('ror', arguments)
+
+class SllInstruction(Instruction):
+    def __init__(self, arguments):
+        super().__init__('sll', arguments)
+
+class SraInstruction(Instruction):
+    def __init__(self, arguments):
+        super().__init__('sra', arguments)
+    
+class SrlInstruction(Instruction):
+    def __init__(self, arguments):
+        super().__init__('srl', arguments)
+
+class XorInstruction(Instruction):
+    def __init__(self, arguments):
+        super().__init__('xor', arguments)
+
+class AbsInstruction(Instruction):
+    def __init__(self, arguments):
+        super().__init__('abs', arguments)
