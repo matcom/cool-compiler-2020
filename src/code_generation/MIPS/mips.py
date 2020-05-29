96a481b79cf43431aecb71addd1988d3b517e2a8
Eric <e.martin@estudiantes.matcom.uh.cu>
2020-05-28 20:46:19 -0400

    Move aux methods to utilities.py

--- src/code_generation/MIPS/mips.py
+++ src/code_generation/MIPS/mips.py
@@ -5,53 +5,6 @@ from .utilities import *
 __METHOD_MAPPING__ = {}
 __ADDRS__ = {}
 
-
-def save_callee_registers():
-    allocate_stack(40)
-    for i in range(8):
-        push_stack(f'$s{i}', (7-i)*4)
-    push_stack('$fp', 4)
-    push_stack('$ra')  
-    
-
-
-def save_caller_registers():
-    allocate_stack(40)
-    for i in range(10):
-        push_stack(f'$t{i}', (9-i)*4)
-
-
-def restore_callee_registers():
-    for i in range(8):
-        push_stack(f'$s{i}', (7-i)*4)
-    peek_stack('$fp', 4)
-    peek_stack('$ra')
-    restore_stack(40)
-
-
-def restore_caller_registers():
-    for i in range(10):
-        peek_stack(f'$t{i}', (9-i)*4)
-    restore_stack(40)
-
-def restore_stack(bytes):
-    return [mips.AdduInstruction('$sp', '$sp', bytes)]
-        
-def allocate_stack(bytes):
-    return [mips.SubuInstruction('$sp', '$sp', bytes)]
-
-def push_stack(src, pos=0):
-    if pos:
-        return [mips.SwInstruction(src, f'{pos}($sp)')]
-    return [mips.SwInstruction(src, '($sp)')]
-
-def peek_stack(src, pos=0):
-    if pos:
-        return [mips.LwInstruction(src, f'{pos}($sp)')]
-    return [mips.LwInstruction(src, '($sp)')]
-        
-
-
 def program_to_mips_visitor(program: cil.ProgramNode):
     global __METHOD_MAPPING__
 
@@ -72,7 +25,7 @@ def program_to_mips_visitor(program: cil.ProgramNode):
         
         
 def function_to_mips_visitor(function):
-    code=save_callee_registers()
+    code=save_callee_registers() #revisar que save_callee_registers no tiene valor de retorno
     for i,param in enumerate(function.params):
         __ADDRS__[param.id]=(len(function.params)-1-i)*4
               
--- src/code_generation/MIPS/utilities.py
+++ src/code_generation/MIPS/utilities.py
@@ -31,3 +31,51 @@ def get_type_size(type):
     the type identifier also.
     """
     return len(type.attributes) + 1
+
+
+def save_callee_registers():
+    allocate_stack(40)
+    for i in range(8):
+        push_stack(f'$s{i}', (7-i)*4)
+    push_stack('$fp', 4)
+    push_stack('$ra')  
+    
+
+def save_caller_registers():
+    allocate_stack(40)
+    for i in range(10):
+        push_stack(f'$t{i}', (9-i)*4)
+
+
+def restore_callee_registers():
+    for i in range(8):
+        push_stack(f'$s{i}', (7-i)*4)
+    peek_stack('$fp', 4)
+    peek_stack('$ra')
+    restore_stack(40)
+
+
+def restore_caller_registers():
+    for i in range(10):
+        peek_stack(f'$t{i}', (9-i)*4)
+    restore_stack(40)
+
+
+def restore_stack(bytes):
+    return [mips.AdduInstruction('$sp', '$sp', bytes)]
+
+
+def allocate_stack(bytes):
+    return [mips.SubuInstruction('$sp', '$sp', bytes)]
+
+
+def push_stack(src, pos=0):
+    if pos:
+        return [mips.SwInstruction(src, f'{pos}($sp)')]
+    return [mips.SwInstruction(src, '($sp)')]
+
+
+def peek_stack(src, pos=0):
+    if pos:
+        return [mips.LwInstruction(src, f'{pos}($sp)')]
+    return [mips.LwInstruction(src, '($sp)')]
