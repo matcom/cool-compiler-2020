7bc76bafae2e8fd1f4e93ef7a5606c42cb54a345
Lazaro Raul <riglesiasvera@gmail.com>
2020-06-10 13:09:12 -0400

    Remove ComputedVisitor

--- src/core/cmp/visitors.py
+++ src/core/cmp/visitors.py
@@ -390,7 +390,7 @@ class TypeChecker:
         self.current_type = self.context.get_type(node.id)
         
         scope.define_variable('self', self.current_type)
-        cur_type = self.current_type
+        cur_type = self.current_type.parent
         while True:
             for attr in cur_type.attributes:
                 scope.define_variable(attr.name, attr.type)
@@ -398,7 +398,18 @@ class TypeChecker:
                 break
             cur_type = cur_type.parent
             
+        cur_type = self.current_type
+        pending, count = [], 0
         for feature in node.features:
+            if isinstance(feature, AttrDeclarationNode):
+                self.visit(feature, scope)
+                if not scope.is_defined(feature.id):
+                    scope.define_variable(feature.id, cur_type.attributes[count].type)
+                    count += 1
+            else:
+                pending.append(feature)
+
+        for feature in pending:  
             self.visit(feature, scope.create_child())
     
     @visitor.when(AttrDeclarationNode)
@@ -1187,136 +1198,3 @@ class InferenceVisitor(TypeChecker):
             
         node.computed_type = node_type
 
-class ComputedVisitor(FormatVisitor):
-    def replace_auto(self, name):
-        return 'Object' if IsAuto(name) else name
-
-    @visitor.on('node')
-    def visit(self, node, tabs):
-        pass
-    
-    @visitor.when(ProgramNode)
-    def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__ProgramNode [<class> ... <class>]'
-        statements = '\n'.join(self.visit(child, tabs + 1) for child in node.declarations)
-        return f'{ans}\n{statements}'
-    
-    @visitor.when(ClassDeclarationNode)
-    def visit(self, node, tabs=0):
-        parent = '' if node.parent is None else f"inherits {node.parent}"
-        ans = '\t' * tabs + f'\\__ClassDeclarationNode: class {node.id} {parent} {{ <feature> ... <feature> }}'
-        features = '\n'.join(self.visit(child, tabs + 1) for child in node.features)
-        return f'{ans}\n{features}'
-    
-    @visitor.when(AttrDeclarationNode)
-    def visit(self, node, tabs=0):
-        sons = [node.expr] if node.expr else []
-        text = '<- <expr>' if node.expr else ''
-        real_type = self.replace_auto(node.scope.find_variable(node.id).type.name)
-        ans = '\t' * tabs + f'\\__AttrDeclarationNode: {node.id} : {real_type} {text}'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in sons)
-        return f'{ans}\n{body}' if body else f'{ans}'
-    
-    @visitor.when(FuncDeclarationNode)
-    def visit(self, node, tabs=0):
-        params = ', '.join(':'.join(param) for param in node.params)
-        real_type = self.replace_auto(node.method.return_type.name)
-        ans = '\t' * tabs + f'\\__FuncDeclarationNode: {node.id}({params}) : {real_type} {{<body>}}'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in node.body)
-        return f'{ans}\n{body}'
-    
-    @visitor.when(IfThenElseNode)
-    def visit(self, node, tabs=0):
-        sons = [node.condition, node.if_body]
-        text = ''
-        if node.else_body:
-            sons.append(node.else_body)
-            text += 'else <body>'
-        ans = '\t' * tabs + f'\\__IfThenElseNode: if <cond> then <body> {text} fi'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in sons)
-        return f'{ans}\n{body}'
-    
-    @visitor.when(WhileLoopNode)
-    def visit(self, node, tabs=0):
-        sons = [node.condition, node.body]
-        ans = '\t' * tabs + f'\\__WhileLoopNode: while <cond> loop <body> pool'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in sons)
-        return f'{ans}\n{body}'
-    
-    @visitor.when(BlockNode)
-    def visit(self, node, tabs=0):
-        sons = node.exprs
-        ans = '\t' * tabs + f'\\__BlockNode: {{<expr> ... <expr>}}'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in sons)
-        return f'{ans}\n{body}'
-    
-    @visitor.when(LetInNode)
-    def visit(self, node, tabs=0):
-        sons = node.let_body + [node.in_body]
-        ans = '\t' * tabs + f'\\__LetInNode: let {{<attr> ... <attr>}} in <expr>'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in sons)
-        return f'{ans}\n{body}'
-    
-    @visitor.when(CaseOfNode)
-    def visit(self, node, tabs=0):
-        sons = [node.expr] + node.branches
-        ans = '\t' * tabs + f'\\__CaseOfNode: case <expr> of {{<case> ... <case>}} esac'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in sons)
-        return f'{ans}\n{body}'
-    
-    @visitor.when(CaseExpressionNode)
-    def visit(self, node, tabs=0):
-        sons = [node.expr]
-        ans = '\t' * tabs + f'\\__CaseExpressionNode: {node.id} : {node.type} => <expr>'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in sons)
-        return f'{ans}\n{body}'
-
-    @visitor.when(LetAttributeNode)
-    def visit(self, node, tabs=0):
-        sons = [node.expr] if node.expr else []
-        text = '<- <expr>' if node.expr else ''
-        real_type = self.replace_auto(node.scope.find_variable(node.id).type.name)
-        ans = '\t' * tabs + f'\\__LetAttributeNode: {node.id} : {real_type} {text}'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in sons)
-        return f'{ans}\n{body}' if body else f'{ans}'
-    
-    @visitor.when(AssignNode)
-    def visit(self, node, tabs=0):
-        sons = [node.expr]
-        ans = '\t' * tabs + f'\\__AssignNode: {node.id} <- <expr>'
-        body = '\n'.join(self.visit(child, tabs + 1) for child in sons)
-        return f'{ans}\n{body}'
-    
-    @visitor.when(UnaryNode)
-    def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__{node.__class__.__name__} <expr>'
-        right = self.visit(node.expr, tabs + 1)
-        return f'{ans}\n{right}'
-   
-    @visitor.when(BinaryNode)
-    def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__<expr> {node.__class__.__name__} <expr>'
-        left = self.visit(node.left, tabs + 1)
-        right = self.visit(node.right, tabs + 1)
-        return f'{ans}\n{left}\n{right}'
-
-    @visitor.when(AtomicNode)
-    def visit(self, node, tabs=0):
-        return '\t' * tabs + f'\\__ {node.__class__.__name__}: {node.lex}'
-    
-    @visitor.when(FunctionCallNode)
-    def visit(self, node, tabs=0):
-        obj = self.visit(node.obj, tabs + 1)
-        ans = '\t' * tabs + f'\\__FunctionCallNode: <obj>.{node.id}(<expr>, ..., <expr>)'
-        args = '\n'.join(self.visit(arg, tabs + 1) for arg in node.args)
-        return f'{ans}\n{obj}\n{args}'
-
-    @visitor.when(MemberCallNode)
-    def visit(self, node, tabs=0):
-        ans = '\t' * tabs + f'\\__MemberCallNode: {node.id}(<expr>, ..., <expr>)'
-        args = '\n'.join(self.visit(arg, tabs + 1) for arg in node.args)
-        return f'{ans}\n{args}'
-    
-    @visitor.when(NewNode)
-    def visit(self, node, tabs=0):
-        return '\t' * tabs + f'\\__NewNode: new {node.type}()'
