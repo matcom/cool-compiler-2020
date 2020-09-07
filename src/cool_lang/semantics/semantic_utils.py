04b1541d217df6e5e6468cd9be3fa5fe66f8e442
cbermudez <cbermudez.dev@gmail.com>
2020-09-07 13:46:31 -0400

    Add features to classes Type and Method
    Class Method can now check if an instance is an override from another. Class Type can now check if an instance is a subtype from another.

--- src/cool_lang/semantics/semantic_utils.py
+++ src/cool_lang/semantics/semantic_utils.py
@@ -20,6 +20,18 @@ class Method:
         self.param_names = param_names
         self.param_types = params_types
         self.return_type = return_type
+    
+    def is_override(self, omethod): # check if self is an override of omethod
+        valid = False
+        if omethod.name == self.name:
+            if self.return_type.is_subtype(omethod.return_type):
+                if len(self.param_names) == len(omethod.param_names):
+                    for ptype, optype in zip(self.param_types, omethod.param_types):
+                        if not ptype.is_subtype(optype): # The manual says it must be exactly the same.
+                            break
+                    else:
+                        valid = True
+        return valid
 
     def __str__(self):
         params = ', '.join(f'{n}:{t.name}' for n,t in zip(self.param_names, self.param_types))
@@ -31,10 +43,13 @@ class Type:
         self.attributes = []
         self.methods = {}
         self.parent = None
+        self._visited = False
 
     def set_parent(self, parent):
         if self.parent is not None:
-            raise SemanticException(f'Parent type is already set for {self.name}.')
+            raise SemanticException(f'Parent type is already set for type {self.name}.')
+        if parent.name in ['Int', 'String', 'Bool']:
+            raise SemanticException(f'Cannot inherit from basic type {parent.name}.')
         self.parent = parent
 
     def get_attribute(self, name:str):
@@ -42,11 +57,11 @@ class Type:
             return next(attr for attr in self.attributes if attr.name == name)
         except StopIteration:
             if self.parent is None:
-                raise SemanticException(f'Attribute "{name}" is not defined in {self.name}.')
+                raise SemanticException(f'Attribute "{name}" is not defined in type {self.name}.')
             try:
                 return self.parent.get_attribute(name)
             except SemanticException:
-                raise SemanticException(f'Attribute "{name}" is not defined in {self.name}.')
+                raise SemanticException(f'Attribute "{name}" is not defined in type {self.name}.')
 
     def define_attribute(self, name:str, typex):
         try:
@@ -56,18 +71,18 @@ class Type:
             self.attributes.append(attribute)
             return attribute
         else:
-            raise SemanticException(f'Attribute "{name}" is already defined in {self.name}.')
+            raise SemanticException(f'Attribute "{name}" is already defined in type {self.name}.')
 
     def get_method(self, name:str):
         try:
             return self.methods[name]
         except KeyError:
             if self.parent is None:
-                raise SemanticException(f'Method "{name}" is not defined in {self.name}.')
+                raise SemanticException(f'Method "{name}" is not defined in type {self.name}.')
             try:
                 return self.parent.get_method(name)
             except SemanticException:
-                raise SemanticException(f'Method "{name}" is not defined in {self.name}.')
+                raise SemanticException(f'Method "{name}" is not defined in type {self.name}.')
 
     def define_method(self, name:str, param_names:list, param_types:list, return_type):
         try:
@@ -75,11 +90,23 @@ class Type:
         except SemanticException:
             pass
         else:
-            if method.return_type != return_type or method.param_types != param_types:
-                raise SemanticException(f'Method "{name}" already defined in {self.name} with a different signature.')
-
+            if name in self.methods.keys(): # duplicate?
+                raise SemanticException(f'Method "{name}" already defined in type {self.name}.')
+            else: # override?
+                new_method = Method(name, param_names, param_types, return_type)
+                if not new_method.is_override(method): # override check is different from the manual, check method if something goes wrong here
+                    raise SemanticException(f'Invalid override of method "{name}" in type {self.name}.')
         method = self.methods[name] = Method(name, param_names, param_types, return_type)
         return method
+
+    def is_subtype(self, otype): # check if self is subtype of otype
+        actual = self
+        while True:
+            if actual.parent == None:
+                return False
+            if actual.name == otype.name:
+                return True
+            actual = actual.parent
 
     def __str__(self):
         output = f'type {self.name}'
@@ -100,6 +127,9 @@ class Type:
 class ErrorType(Type):
     def __init__(self):
         Type.__init__(self, '<error>')
+
+    def is_subtype(self, otype):
+        return True
 
     def __eq__(self, other):
         return isinstance(other, Type)
