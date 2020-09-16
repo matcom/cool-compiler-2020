4e4f892bb0cec2732926188f220a00c426cba82e
Lazaro Raul <riglesiasvera@gmail.com>
2020-09-16 12:44:31 -0400

    [verifier] - Add a new visitor
    This visitor is for checking after the inference process whether all the types are still well formed

--- src/core/cmp/visitors.py
+++ src/core/cmp/visitors.py
@@ -1038,3 +1038,43 @@ class InferenceVisitor(TypeChecker):
         if update_condition(right, left) and left in [INT, BOOL, STRING]:
             self.update(node.right, scope, left)
         
+# Type Verifier
+class TypeVerifier:
+    def __init__(self, context, errors=[]):
+        self.context = context
+        self.current_type = None
+        self.errors = errors
+    
+    @visitor.on('node')
+    def visit(self, node):
+        pass
+    
+    @visitor.when(ProgramNode)
+    def visit(self, node):
+        main_token = None
+        for def_class in node.declarations:
+            self.visit(def_class)         
+    
+    @visitor.when(ClassDeclarationNode)
+    def visit(self, node):
+        self.current_type = self.context.get_type(node.id)
+        
+        for feature in node.features:
+            self.visit(feature)
+            
+    @visitor.when(AttrDeclarationNode)
+    def visit(self, node):
+        pass
+        
+    @visitor.when(FuncDeclarationNode)
+    def visit(self, node):
+        try:
+            m1 = node.method
+            m2 = self.current_type.parent.get_method(m1.name)
+            assert m1.return_type == m2.return_type and m1.param_types == m2.param_types
+        except AttributeError: 
+            pass
+        except SemanticError:
+            pass
+        except AssertionError:
+            self.errors.append((f'Method "{m1.name}" already defined in {self.current_type.name} with a different signature.', node.tid))
