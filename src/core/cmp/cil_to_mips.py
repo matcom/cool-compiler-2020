f93f59f67fb062f7aab9814ca356b387566bbecc
Mauricio Perdomo <ml.perdomo97@gmail.com>
2020-09-23 23:16:28 -0400

    [mips] Add cil.LessNode case to visit function of cil_to_mips visitor

--- src/core/cmp/cil_to_mips.py
+++ src/core/cmp/cil_to_mips.py
@@ -765,6 +765,31 @@ class CILToMIPSVisitor:
 
         return instructions
 
+    @visitor.when(cil.LessNode)
+    def visit(self, node):
+        instructions = []
+        #Save $a0, $a1, $v0
+
+        if type(node.left) == int:
+            instructions.append(mips.LoadInmediateNode(reg1, node.left))
+        else:
+            left_location = self.get_var_location(node.left)
+            instructions.append(mips.LoadWordNode(mips.ARG_REGISTERS[0], left_location))
+        
+        if type(node.right) == int:
+            instructions.append(mips.LoadInmediateNode(reg2, node.right))
+        else:
+            right_location = self.get_var_location(node.right)
+            instructions.append(mips.LoadWordNode(mips.ARG_REGISTERS[1], right_location))
+        
+        instruction.append(mips.JumpAndLinkNode('less'))
+        dest_location = self.get_var_location(node.dest)
+        instruction.append(mips.StoreWordNode(mips.V0_REG, dest_location))
+
+        return instructions
+    
+
+
         
     
 
