c0907b9eba9d98b44073c54778db84b931bc6205
AntiD2ta <mtpotro41@gmail.com>
2020-10-03 12:55:02 -0400

    [cil] - Fix value left in `scope.ret_expr` in Minus, Star, and Divide Nodes
    Replace vname for instance variable

--- src/core/cmp/cool_to_cil.py
+++ src/core/cmp/cool_to_cil.py
@@ -658,7 +658,7 @@ class COOLToCILVisitor(BaseCOOLToCILVisitor):
         instance = self.define_internal_local()
         self.register_instruction(cil.ArgNode(vname))
         self.register_instruction(cil.StaticCallNode(self.to_function_name('init', 'Int'), instance))
-        scope.ret_expr = vname
+        scope.ret_expr = instance
 
     @visitor.when(cool.StarNode)
     def visit(self, node, scope):
@@ -677,7 +677,7 @@ class COOLToCILVisitor(BaseCOOLToCILVisitor):
         instance = self.define_internal_local()
         self.register_instruction(cil.ArgNode(vname))
         self.register_instruction(cil.StaticCallNode(self.to_function_name('init', 'Int'), instance))
-        scope.ret_expr = vname
+        scope.ret_expr = instance
 
     @visitor.when(cool.DivNode)
     def visit(self, node, scope):
@@ -702,7 +702,7 @@ class COOLToCILVisitor(BaseCOOLToCILVisitor):
         instance = self.define_internal_local()
         self.register_instruction(cil.ArgNode(vname))
         self.register_instruction(cil.StaticCallNode(self.to_function_name('init', 'Int'), instance))
-        scope.ret_expr = vname
+        scope.ret_expr = instance
 
     @visitor.when(cool.IsVoidNode)
     def visit(self, node, scope):
