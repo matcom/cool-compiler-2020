31f1c26668c74323d13fa6b34291a0d3a438bd19
daly <dalianysperez@gmail.com>
2020-10-05 12:04:20 -0400

    Fixing topologic sort

--- src/semantic_analyzer.py
+++ src/semantic_analyzer.py
@@ -11,7 +11,7 @@ LOCAL_ALREADY_DEFINED = 'Variable "%s" is already defined in method "%s".'
 INCOMPATIBLE_TYPES = 'Cannot convert "%s" into "%s".'
 VARIABLE_NOT_DEFINED = 'Variable "%s" is not defined in "%s".'
 INVALID_OPERATION = 'Operation is not defined between "%s" and "%s".'
-BUILINT_TYPES = ['Object', 'Int', 'String', 'Bool', 'IO']
+BUILTIN_TYPES = ['Object', 'Int', 'String', 'Bool', 'IO']
 
 class TypeCollector(object):
     def __init__(self, errors=[]):
@@ -27,7 +27,7 @@ class TypeCollector(object):
         self.context = Context()
         self.context.create_builtin_types()
         for klass in node.classes:
-            if klass.name in BUILINT_TYPES:
+            if klass.name in BUILTIN_TYPES:
                 self.errors.append("Is an error redefine a builint type")
             else:
                 self.visit(klass)
@@ -46,41 +46,42 @@ class TypeBuilder:
         self.current_type = None
         self.errors = errors
         self.sort = []  #topologic sort for all types defined 
-        self.visited = {} #types visited in the graph by the DFS
-        for t in self.context.graph.keys():
-            self.visited[t] = False
-
-    def detect_cycles(self):
-        self.visit_component('Object')
-        for t in self.visited.keys():
-            if not self.visited[t]: # type t is part of another component in the graph 
-                self.errors.append("The graph of types is not a tree")
-                return
-                
-    def visit_component(self, node):
-        if not self.visited[node]: 
-            self.visited[node] = True
-            for t in self.context.graph[node]:
-                    self.visit_component(t)
-
-    def topologic_sort(self, actual_type):
+        self.visited = {key: False for key in self.context.graph.keys()} #types visited in the graph by the DFS
+       
+    def visit_component(self, actual_type):
         self.sort.append(actual_type)
         for children in self.context.graph[actual_type]:
-            self.topologic_sort(children)
+            self.visit_component(children)
 
+    def topologic_sort(self):
+        indeg = {key: 0 for key in self.context.graph.keys()}
+        for u in self.context.graph.keys():
+            for v in self.context.graph[u]:
+                indeg[v] += 1
+        
+        roots = [key for key in indeg.keys() if indeg[key] == 0]
+        if len(roots) > 1:
+            self.errors.append("The graph of types is not a tree")
+        for v in roots:
+            self.visit_component(v)
 
+        
     @visitor.on('node')
     def visit(self, node):
         pass
 
     @visitor.when(AST.Program)
     def visit(self, node):
-        self.detect_cycles()
-        self.topologic_sort('Object')
+        self.topologic_sort()
         for t in self.sort:
-            if t not in BUILINT_TYPES:
-                class_node = self.context.classes[t]
-                self.visit(class_node)
+            if t not in BUILTIN_TYPES:
+                try:
+                    class_node = self.context.classes[t]
+                except KeyError:
+                    self.errors.append("The class {} not exist".format(t))
+                else:
+                    self.visit(class_node)
+                
               
     @visitor.when(AST.Class)
     def visit(self, node):
@@ -308,10 +309,10 @@ class SemanticAnalyzer:
         collector.visit(self.ast)
         context = collector.context
 
+
         # #'=============== BUILDING TYPES ================'
         builder = TypeBuilder(context, self.errors)
         builder.visit(self.ast)
-
 
         #'=============== CHECKING TYPES ================'
         # checker = TypeChecker(context, self.errors)
