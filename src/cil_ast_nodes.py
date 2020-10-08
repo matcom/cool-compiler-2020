67b9692aff13e85d7a07d62a6a0c491d6db4b5b5
Ginrod <gilbecubano@gmail.com>
2020-10-08 10:42:42 -0400

    adding first CIL AST Nodes, visitor class for teh creation of Types, Data and Code Sections

---
+++ src/cil_ast_nodes.py
@@ -0,0 +1,28 @@
+class AST:
+    def __init__(self):
+        pass
+
+class Program(AST):
+    def __init__(self):
+        super(Program, self).__init__()
+        self.types = [] # list of AST.Type
+        self.data = [] # list of AST.Data
+        self.code = [] # list of AST.Function
+
+class Type(AST):
+    def __init__(self, name):
+        super(Type, self).__init__()
+        self.name = name
+        self.attributes = []
+        self.methods = []
+
+class Attribute(AST):
+    def __init__(self, name):
+        super(Attribute, self).__init__()
+        self.name = name
+
+class Method(AST):
+    def __init__(self, name):
+        super(Method, self).__init__()
+        self.method_name = name
+        self.cil_function = None
