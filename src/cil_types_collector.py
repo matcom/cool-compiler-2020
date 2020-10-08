58c07ac4a0820d4eac1f4ee5339186644ae0b615
Ginrod <gilbecubano@gmail.com>
2020-10-08 18:47:58 -0400

    fixing CIL visit calls, adding first node visit

--- src/cil_types_collector.py
+++ src/cil_types_collector.py
@@ -6,3 +6,13 @@ class CILTypesCollector:
     def __init__(self, cil_ast, context):
         self.cil_ast = cil_ast
         self.context = context
+    
+    @visitor.on('node')
+    def visit(self, node):
+        pass
+
+    @visitor.when(COOL_AST.Program)
+    def visit(self, node):
+        for name, type in self.context.types.items():
+            x = 0
+        # pass
