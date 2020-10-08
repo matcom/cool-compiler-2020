f6e36366c394158e93a02a157cb7f29740191702
Ginrod <gilbecubano@gmail.com>
2020-10-08 18:16:41 -0400

    creating cil_ast Program node and passing it to CIL visitors classes

--- src/cil_types_collector.py
+++ src/cil_types_collector.py
@@ -3,6 +3,5 @@ import ast_nodes as COOL_AST
 import cil_ast_nodes as CIL_AST
 
 class CILTypesCollector:
-    def __init__(self):
-        pass
-
+    def __init__(self, cil_ast):
+        self.cil_ast = cil_ast
