287b686cc5114079fd1413051129c6ee2d36834f
Ginrod <gilbecubano@gmail.com>
2020-10-08 12:56:30 -0400

    fixing run-time errors in ast_nodes.py and syntax errors in cil related scripts

--- src/cil_data_collector.py
+++ src/cil_data_collector.py
@@ -2,7 +2,7 @@ import visitor
 import ast_nodes as COOL_AST
 import cil_ast_nodes as CIL_AST
 
-class CILDataCollector
+class CILDataCollector:
     def __init__(self):
         pass
 
--- src/cil_types_collector.py
+++ src/cil_types_collector.py
@@ -2,7 +2,7 @@ import visitor
 import ast_nodes as COOL_AST
 import cil_ast_nodes as CIL_AST
 
-class CILTypesCollector
+class CILTypesCollector:
     def __init__(self):
         pass
 
