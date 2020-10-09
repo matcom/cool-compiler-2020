f855883490f0a852b14c0fd2ee27fd62955a787e
Ginrod <gilbecubano@gmail.com>
2020-10-09 12:25:15 -0400

    creating CIL Attribute node with COOL Attribute name

--- src/cil_types_collector.py
+++ src/cil_types_collector.py
@@ -16,8 +16,8 @@ class CILTypesCollector:
         for name, type in self.context.types.items():
             if name in ['ErrorType', 'SELF_TYPE']:
                 continue
-            
-            cil_attributes = [CIL_AST.Attribute(attr) for attr in type.get_all_attributes()]
+
+            cil_attributes = [CIL_AST.Attribute(attr.name) for attr in type.get_all_attributes()]
             cil_methods = [CIL_AST.Method(method, f'func_{kclass}_{method}') for kclass, method in type.get_all_methods()]
             cil_type = CIL_AST.Type(name, cil_attributes, cil_methods)
             self.cil_ast.types.append(cil_type)
