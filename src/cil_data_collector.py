ae9e8cd1c65cb722a255c1491a8ce6de960cdc3c
Ginrod <gilbecubano@gmail.com>
2020-10-09 00:13:23 -0400

    completing cil DATA section mapping

--- src/cil_data_collector.py
+++ src/cil_data_collector.py
@@ -7,3 +7,32 @@ class CILDataCollector:
         self.cil_ast = cil_ast
         self.context = context
 
+    @visitor.on('node')
+    def visit(self, node):
+        pass
+
+    @visitor.when(COOL_AST.Program)
+    def visit(self, node):
+        self.cil_ast.data["Object"] = "object_type_name"
+        
+        for klass in node.classes:
+            self.visit(klass)
+    
+    @visitor.when(COOL_AST.Class)
+    def visit(self, node):
+        for feature in node.features:
+            self.visit(feature)
+    
+    @visitor.when(COOL_AST.ClassMethod)
+    def visit(self, node):
+        self.visit(node.expr)
+
+    @visitor.when(COOL_AST.DynamicCall)
+    def visit(self, node):
+        for arg in node.args:
+            self.visit(arg)
+    
+    @visitor.when(COOL_AST.STRING)
+    def visit(self, node):
+        if node.value not in self.cil_ast.data:
+            self.cil_ast.data[node.value] = f's{len(self.cil_ast.data)}'
