de48b2df6930cb01a234e9f13ea5c6e583e409ac
Harold Rosales <h.rosales@estudiantes.matcom.uh.cu>
2020-11-04 21:51:12 -0500

    operation nodes definition

---
+++ src/code_generation/nodesIL/node_il.py
@@ -0,0 +1,4 @@
+
+class NodeIL():
+    pass
+
--- src/code_generation/nodesIL/operation_node_il.py
+++ src/code_generation/nodesIL/operation_node_il.py
@@ -2,16 +2,21 @@ from .node_il import NodeIL
 
 class BinaryOperationIL(NodeIL):
     
-    def __init__(self):
-        pass
+    def __init__(self, var, leftOp, rightOp, op):
+        self.var = var
+        self.leftOp = leftOp
+        self.rightOp = rightOp
+        self.op = op
     
     def __str__(self):
-        pass
+        return "{} = {} {} {}\n".format(self.var, self.leftOp, self.op, self.rightOp)
 
 class UnaryOperationIL(NodeIL):
     
-    def __init__(self):
-        pass
+    def __init__(self, var, op, symb):
+        self.var = var
+        self.op = op
+        self.symb = symb
     
     def __str__(self):
-        pass
+        return "{} = {} {}".format(self.var, self.symbol, self.op)
