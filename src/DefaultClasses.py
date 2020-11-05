803b0ad8bbef1f863b96c7ede1ebd42cb7ee5de5
TTigre <tigrejg98@gmail.com>
2020-11-05 18:01:26 -0500

    -Agregada clase String inicial -Agregado método init a Object, para inclusión de los atributos

--- src/DefaultClasses.py
+++ src/DefaultClasses.py
@@ -6,6 +6,7 @@ class Defaults:
         abort=MethodNode("abort",[],"Object",[])
         typename=MethodNode("type_name",[],"String",[])
         copy=MethodNode("copy",[],"SELF_TYPE",[])
+        init=MethodNode("$init",[],"SELF_TYPE",[])
 
         clase=ClassNode("Object",None,[identifier,abort,typename,copy])
 
@@ -13,7 +14,7 @@ class Defaults:
     
     def IOClass():
         identifier=AttributeNode("type#","Int",IntegerNode(1))
-        out_string=MethodNode("out_string",[ParameterNode("x","String"),"SELF_TYPE",[]],"SELF_TYPE",[])
+        out_string=MethodNode("out_string",[ParameterNode("x","String")],"SELF_TYPE",[])
         out_int=MethodNode("out_int",[ParameterNode("x","Int")],"SELF_TYPE",[])
         in_string=MethodNode("in_string",[],"String",[])
         in_int=MethodNode("in_int",[],"Int",[])
@@ -21,3 +22,13 @@ class Defaults:
         clase=ClassNode("IO","Object",[identifier,out_string,out_int,in_string,in_int])
 
         return clase
+
+    def StringClass():
+        identifier=AttributeNode("type#","Int",IntegerNode(2))
+        value=AttributeNode("value", "Int", IntegerNode(0))
+
+        length=MethodNode("length",[],"Int",[])
+        concat=MethodNode("concat",[ParameterNode("s","String")],"String",[])
+        substr=MethodNode("substr",[ParameterNode("i","Int"),ParameterNode("l","Int")],"String",[])
+
+        clase=ClassNode("String","Object",[identifier,value,length,concat,substr])
