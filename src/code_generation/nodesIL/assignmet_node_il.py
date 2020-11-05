bff5734cf89c70175a0dcbfea0bf088d2f27d4ec
Harold Rosales <h.rosales@estudiantes.matcom.uh.cu>
2020-11-04 21:58:17 -0500

    assignments node definitions

--- src/code_generation/nodesIL/assignmet_node_il.py
+++ src/code_generation/nodesIL/assignmet_node_il.py
@@ -2,40 +2,44 @@ from .node_il import NodeIL
 
 class AssignmentNodeIL(NodeIL):
 
-    def __init__(self):
-        pass
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
 
     def __str__(self):
-        pass
+        return "{} = {}".format(self.left, self.right)
 
 class VarToVarIL(AssigmentNodeIL):
 
-    def __init__(self):
-        pass
+    def __init__(self, left, right):
+        super().__init__(left, right)
     
     def __str__(self):
-        pass
+        return "{} = {}".format(self.left, self.right)
 
 class MemoToVarIL(AssigmentNodeIL):
     
     def __init__(self, left, right, offset):
-        pass
+        super().__init__(left, right)
+        self.offset = offset
     
     def __str__(self):
-        pass
+        return "{} = {}".format(self.left, self.offset)
 
 class VarToMemoIL(AssigmentNodeIL):
     
     def __init__(self, left, right, offset):
-        pass
+        super().__init__(left, right)
+        self.offset = offset
     
     def __str__(self):
-        pass
+        return "{} = {}".format(self.offset, self.right)
 
 class ConstToMemoIL(AssigmentNodeIL):
     
     def __init__(self, left, right, offset):
-        pass
+        super().__init__(left, right)
+        self.offset = offset
     
     def __str__(self):
-        pass
+        return "{} = {}\n".format(self.left + self.offset, self.right)
