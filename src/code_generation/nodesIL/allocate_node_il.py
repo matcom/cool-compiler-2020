4074a869c89f296b1d9b9d03754c1f163a5d5491
Harold Rosales <h.rosales@estudiantes.matcom.uh.cu>
2020-11-05 21:25:21 -0500

    some allocate IL

--- src/code_generation/nodesIL/allocate_node_il.py
+++ src/code_generation/nodesIL/allocate_node_il.py
@@ -2,8 +2,10 @@ from .node_il import NodeIL
 
 class AllocateIL(NodeIL):
     
-    def __init__(self):
-        pass
+    def __init__(self, var, size, type):
+        self.var = var
+        self.size = size
+        self.type = type
 
     def __str__(self):
         pass
