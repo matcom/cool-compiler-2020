a67b3f9cabd237880b9ee954aac9657f8df664a1
Harold Rosales <h.rosales@estudiantes.matcom.uh.cu>
2020-11-06 10:39:45 -0500

    variables class completed missing some test

--- src/code_generation/nodesIL/variables.py
+++ src/code_generation/nodesIL/variables.py
@@ -1,28 +1,36 @@
 class Variables:
     
     def __init__(self):
-        self.variables = {}
+        self.stack = {}
         self.vars = []
 
     def add_var(self, name):
-        pass
+        self.stack[name] = len(self.stack) + 1
+        self.vars.append(name)
 
     def id(self, name):
-        pass
+        return len(self.stack) - self.stack[name] + 1
 
     def pop_var(self):
-        pass
+        self.stack.pop(self.vars[-1])
+        self.vars.pop()
 
     def add_temp(self):
-        pass
+        name = len(self.stack) + 1
+        self.add_var('tmp_' + str(name))
 
     def peek_last(self):
-        pass
+        return self.vars[-1]
 
     def get_stack(self):
-        pass
+        stack = '|'
+        for v in self.stack:
+            stack += 'id: ' + str(self.id(v) ) + 'name :' + v + '|'
+        return stack
 
     def get_copy(self):
-        pass
+        vars_copy = Variables()
+        vars_copy.stack = self.stack.copy()
+        vars_copy.vars = self.vars.copy()
+        return vars_copy
 
-    
