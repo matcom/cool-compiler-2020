4a17a51e195bed7ae643d803adf55351ba91e867
Harold Rosales <h.rosales@estudiantes.matcom.uh.cu>
2020-11-07 15:05:26 -0500

    finishing add method functionality in virtual table

--- src/code_generation/virtual_table.py
+++ src/code_generation/virtual_table.py
@@ -6,16 +6,38 @@ class VirtualTable:
         self.attributes = {}
 
     def add_method(self, A, B, args):
-        pass
+        
+        if not A in self.methods:
+            self.methods[A] = []
+            self.methos[A].append((B, args))
+        else:
+            add = True
+            for name, arguments in self.methods[A]:
+                if name == B:
+                    if len(args) == len(arguments):
+                        differs = False
+                        for i in range(0, len(args)):
+                            if arg[i] != arguments[i]:
+                                differs = True
+                                break
+                        if !differs:
+                            add = False
+            if add:
+                self.methods[A].append((B, args))                        
+
+        
 
     def add_attr(self, claSS, args):
-        pass
+        if not claSS in self.attributes:
+            self.attributes[claSS] = []
+        for arg in args:
+            self.attributes[claSS].append(a)
 
     def get_method_id(self, claSS, method):
         pass
 
     def get_attributes(self, claSS):
-        pass
+        return self.attributes[claSS]
 
     def get_attributes_id(self, claSS, attr):
         pass
