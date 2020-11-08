8ed3e42489541eab52de747a1291c93c52efeaa6
Harold Rosales <h.rosales@estudiantes.matcom.uh.cu>
2020-11-07 21:55:52 -0500

    starting visitor skeleton for transpilator

---
+++ src/code_generation/nodesIL/__init__.py
@@ -0,0 +1,4 @@
+from node_il import NodeIL
+from allocate_node_il import *
+from assignmet_node_il import *
+from operation_node_il import *
--- src/code_generation/transpilator.py
+++ src/code_generation/transpilator.py
@@ -1,11 +1,9 @@
 import sys
 sys.path.append('/..')
-from .nodesIL.node_il import *
-from .nodesIL.allocate_node_il import *
-from .nodesIL.assignmet_node_il import *
-from .nodesIL.methods_node_il import *
-from .nodesIL.operation_node_il import *
+from .nodesIL import *
+from .virtual_table import VirtualTable
 from ..visitors import visitor
+from ..cl_ast import *
 
 class codeVisitor:
 
@@ -14,4 +12,100 @@ class codeVisitor:
         self.code = []
         self.data = []
 
-        self
+        self.count = 0
+
+        self.virtual_table = VirtualTable()
+
+    def getInt(self):
+        self.count = self.count + 1
+        return self.count 
+
+    def collectTypes(self):
+        pass
+
+    def setInitialCode(self):
+        pass
+    
+    
+    @visitor.on('node')
+    def visit(self, node):
+        pass
+    
+    #program
+    @visitor.on(ProgramNode)
+    def visit(self, node):
+        pass
+    
+    #declarations
+    @visitor.on(ClassDeclarationNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(AttrDeclarationNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(FuncDeclarationNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(VarDeclarationNode)
+    def visit(self, node):
+        pass
+
+    #operations: binary
+    @visitor.on(SumNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(DiffNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(StarNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(DivNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(LessNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(LessEqualNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(EqualNode)
+    def visit(self, node):
+        pass
+
+    #operations: unary
+
+    @visitor.on(BitNotNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(NotNode)
+    def visit(self, node):
+        pass
+
+    #operations: complex
+    #TODO
+
+    #constants
+    @visitor.on(IntegerNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(StringNode)
+    def visit(self, node):
+        pass
+
+    @visitor.on(BoolNode)
+    def visit(self, node):
+        pass
+
+
