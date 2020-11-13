a9608385cc4e63f9d60afbc00f55f325477e4f41
Ginrod <gilbecubano@gmail.com>
2020-11-12 23:01:23 -0500

    fix copy

--- src/cil_to_mips.py
+++ src/cil_to_mips.py
@@ -318,7 +318,7 @@ class CILToMIPSVisitor():
         # offset = self.var_offset[self.current_function.name][self.current_function.params[0].name] 
         self_offset = self.var_offset[self.current_function.name][node.type]
         self.text += f'lw $t0, {self_offset}($sp)\n'  # get self address
-        self.text += f'lw $a0, 4($t0)\n'  # get self size
+        self.text += f'lw $a0, 8($t0)\n'  # get self size
         self.text += f'li $v0, 9\n'
         self.text += f'syscall\n'
         self.text += f'move $t1, $v0\n'
@@ -330,18 +330,23 @@ class CILToMIPSVisitor():
 
         self.text += 'li $a0, 0\n'
         self.text += 'copy_object_word:\n'
-        self.text += 'lw $t2, $a0($t0)\n' # load current object word
-        self.text += 'sw $t2, $a0($t1)\n' # store word in copy object
-        self.text += 'addi $a0, $a0, 4\n' # move to the next word
+        self.text += 'lw $t2, ($t0)\n' # load current object word
+        self.text += 'sw $t2, ($t1)\n' # store word in copy object
+        self.text += 'addi $t0, $t0, 4\n' # move to the next word in orginal object
+        self.text += 'addi $t1, $t1, 4\n' # move to the next word in copy object
+        self.text += 'addi $a0, $a0, 4\n' # size count
         '''
         Src2 can either be a register or an immediate value (a 16 bit integer).
         blt Rsrc1, Src2, label (Branch on Less Than)
         Conditionally branch to the instruction at the label if the contents of register Rsrc1 are less than Src2.
         '''
-        self.text += 'blt $a0, 4($t0), copy_object_word\n' # 4($t0) is the orginal object size
+        self.text += 'blt $a0, 8($t0), copy_object_word\n' # 8($t0) is the orginal object size
 
         offset = self.var_offset[self.current_function.name][node.local_dest]
-        self.text += f'sw $t1, {offset}($sp)\n'  #store instance address in local
+        # $t1 is pointing at the end of the object
+        # if $v0 is modified for any reason (it should not, but...)
+        # before looping we can move $t3, $t1 and use $t3 but this should work 
+        self.text += f'sw $v0, {offset}($sp)\n'  #store instance address in local
 
 if __name__ == '__main__':
     import sys
