56cef2e355aac948e0e353beec346b7a23cad62d
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2020-11-18 20:42:54 -0500

    Update parser rules

--- src/parser.py
+++ src/parser.py
@@ -141,8 +141,8 @@ def p_not_form(p):
 
 
 def p_mixed_expression(p):
-    '''mixed_expression : mixed_expression LESSEQUAL expression
-                        | mixed_expression LESS expression
+    '''mixed_expression : mixed_expression LESSEQUAL arithmetic_expression
+                        | mixed_expression LESS arithmetic_expression
                         | mixed_expression EQUAL expression
                         | arithmetic_expression'''
     if len(p) > 2:
