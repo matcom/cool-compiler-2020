24bb3813732de6c0df12ae5829bc9e766b382710
d.garcia <d.garciabfd@gmail.com>
2020-11-23 03:33:40 -0500

    Add codegen tests

--- tests/codegen_test.py
+++ tests/codegen_test.py
@@ -12,4 +12,5 @@ tests = [(file) for file in os.listdir(tests_dir) if file.endswith('.cl')]
 @pytest.mark.run(order=4)
 @pytest.mark.parametrize("cool_file", tests)
 def test_codegen(compiler_path, cool_file):
-    compare_outputs(compiler_path, tests_dir + cool_file, tests_dir + cool_file[:-3] + '_output.txt')
+    compare_outputs(compiler_path, tests_dir + cool_file, tests_dir + cool_file[:-3] + '_input.txt',\
+        tests_dir + cool_file[:-3] + '_output.txt')
