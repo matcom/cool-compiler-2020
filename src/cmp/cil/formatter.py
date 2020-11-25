db4f74a7843254507a79adba9e8fbf4a3d65b444
leynier <leynier41@gmail.com>
2020-11-25 16:10:16 -0500

    Fix CIL formatter

--- src/cmp/cil/formatter.py
+++ src/cmp/cil/formatter.py
@@ -1,187 +1,233 @@
-from .ast import ProgramNode, TypeNode, FunctionNode, ParamNode, LocalNode, AssignNode, PlusNode \
-    , MinusNode, StarNode, DivNode, AllocateNode, TypeOfNode, StaticCallNode, DynamicCallNode    \
-    , ArgNode, ReturnNode, ReadStrNode, ReadIntNode, PrintStrNode, PrintIntNode, LengthNode, ConcatNode, PrefixNode     \
-    , SubstringNode, GetAttribNode, SetAttribNode, LabelNode, GotoNode, GotoIfNode    \
-    , DataNode, LessNode, LessEqNode, ComplementNode, IsVoidNode, EqualNode, ConformNode         \
-    , CleanArgsNode, ErrorNode, CopyNode, TypeNameNode, StringEqualNode
+from .ast import (
+    AllocateNode,
+    ArgNode,
+    AssignNode,
+    CleanArgsNode,
+    ComplementNode,
+    ConcatNode,
+    ConformNode,
+    CopyNode,
+    DataNode,
+    DivNode,
+    DynamicCallNode,
+    EqualNode,
+    ErrorNode,
+    FunctionNode,
+    GetAttribNode,
+    GotoIfNode,
+    GotoNode,
+    IsVoidNode,
+    LabelNode,
+    LengthNode,
+    LessEqNode,
+    LessNode,
+    LocalNode,
+    MinusNode,
+    ParamNode,
+    PlusNode,
+    PrefixNode,
+    PrintIntNode,
+    PrintStrNode,
+    ProgramNode,
+    ReadIntNode,
+    ReadStrNode,
+    ReturnNode,
+    SetAttribNode,
+    StarNode,
+    StaticCallNode,
+    StringEqualNode,
+    SubstringNode,
+    TypeNameNode,
+    TypeNode,
+    TypeOfNode,
+)
 from .utils import on, when
 
 
 class CIL_FORMATTER(object):
-    @on('node')
+    @on("node")
     def visit(self, node):
         pass
 
     @when(ProgramNode)
-    def visit(self, node: ProgramNode):
-        dottypes = '\n'.join(self.visit(t) for t in node.dottypes)
-        dotdata = '\n'.join(self.visit(t) for t in node.dotdata)
-        dotcode = '\n'.join(self.visit(t) for t in node.dotcode)
+    def visit(self, node: ProgramNode):  # noqa:F811
+        dottypes = "\n".join(self.visit(t) for t in node.dottypes)
+        dotdata = "\n".join(self.visit(t) for t in node.dotdata)
+        dotcode = "\n".join(self.visit(t) for t in node.dotcode)
 
-        return f'.TYPES\n{dottypes}\n\n.DATA\n{dotdata}\n\n.CODE\n{dotcode}'
+        return f".TYPES\n{dottypes}\n\n.DATA\n{dotdata}\n\n.CODE\n{dotcode}"
 
     @when(DataNode)
-    def visit(self, node: DataNode):
+    def visit(self, node: DataNode):  # noqa:F811
         return f'{node.name} = "{node.value}"'
 
     @when(TypeNode)
-    def visit(self, node: TypeNode):
-        features = '\n\t'.join(f'attribute {feature}' if isinstance(feature, str) else f'method {feature[0]}: {feature[1]}' for feature in node.features)
-        
-        return f'type {node.name} {{\n\t{features}\n}}'
+    def visit(self, node: TypeNode):  # noqa:F811
+        features = "\n\t".join(
+            f"attribute {feature}"
+            if isinstance(feature, str)
+            else f"method {feature[0]}: {feature[1]}"
+            for feature in node.features
+        )
+
+        return f"type {node.name} {{\n\t{features}\n}}"
 
     @when(FunctionNode)
-    def visit(self, node: FunctionNode):
-        params = '\n\t'.join(self.visit(x) for x in node.params)
-        localvars = '\n\t'.join(self.visit(x) for x in node.localvars)
-        instructions = '\n\t'.join(self.visit(x) for x in node.instructions)
+    def visit(self, node: FunctionNode):  # noqa:F811
+        params = "\n\t".join(self.visit(x) for x in node.params)
+        localvars = "\n\t".join(self.visit(x) for x in node.localvars)
+        instructions = "\n\t".join(self.visit(x) for x in node.instructions)
 
-        return f'function {node.name} {{\n\t{params}\n\n\t{localvars}\n\n\t{instructions}\n}}'
+        result = f"function {node.name} {{\n\t{params}\n\n\t"
+        result += f"{localvars}\n\n\t{instructions}\n}}"
+        return result
 
     @when(ParamNode)
-    def visit(self, node: ParamNode):
-        return f'PARAM {node.name}'
+    def visit(self, node: ParamNode):  # noqa:F811
+        return f"PARAM {node.name}"
 
     @when(LocalNode)
-    def visit(self, node: LocalNode):
-        return f'LOCAL {node.name}'
+    def visit(self, node: LocalNode):  # noqa:F811
+        return f"LOCAL {node.name}"
 
     @when(CopyNode)
-    def visit(self, node: CopyNode):
-        return f'{node.dest} = COPY {node.obj}'
+    def visit(self, node: CopyNode):  # noqa:F811
+        return f"{node.dest} = COPY {node.obj}"
 
     @when(TypeNameNode)
-    def visit(self, node: TypeNameNode):
-        return f'{node.dest} = TYPENAME {node.type}'
+    def visit(self, node: TypeNameNode):  # noqa:F811
+        return f"{node.dest} = TYPENAME {node.type}"
 
     @when(ErrorNode)
-    def visit(self, node: ErrorNode):
-        return f'ERROR {node.error}'
+    def visit(self, node: ErrorNode):  # noqa:F811
+        return f"ERROR {node.error}"
 
     @when(AssignNode)
-    def visit(self, node: AssignNode):
-        return f'{node.dest} = {node.source}'
+    def visit(self, node: AssignNode):  # noqa:F811
+        return f"{node.dest} = {node.source}"
 
     @when(ConformNode)
-    def visit(self, node: ConformNode):
-        return f'{node.dest} = COMFORM {node.obj} {node.type}'
+    def visit(self, node: ConformNode):  # noqa:F811
+        return f"{node.dest} = COMFORM {node.obj} {node.type}"
 
     @when(IsVoidNode)
-    def visit(self, node: IsVoidNode):
-        return f'{node.dest} = ISVOID {node.body}'
+    def visit(self, node: IsVoidNode):  # noqa:F811
+        return f"{node.dest} = ISVOID {node.body}"
 
     @when(ComplementNode)
-    def visit(self, node: ComplementNode):
-        return f'{node.dest} = COMPLEMENT {node.body}'
+    def visit(self, node: ComplementNode):  # noqa:F811
+        return f"{node.dest} = COMPLEMENT {node.body}"
 
     @when(LessNode)
-    def visit(self, node: LessNode):
-        return f'{node.dest} = {node.left} < {node.right}'
-        
+    def visit(self, node: LessNode):  # noqa:F811
+        return f"{node.dest} = {node.left} < {node.right}"
+
     @when(EqualNode)
-    def visit(self, node: EqualNode):
-        return f'{node.dest} = {node.left} == {node.right}'
+    def visit(self, node: EqualNode):  # noqa:F811
+        return f"{node.dest} = {node.left} == {node.right}"
 
     @when(LessEqNode)
-    def visit(self, node: LessEqNode):
-        return f'{node.dest} = {node.left} <= {node.right}'
+    def visit(self, node: LessEqNode):  # noqa:F811
+        return f"{node.dest} = {node.left} <= {node.right}"
 
     @when(PlusNode)
-    def visit(self, node: PlusNode):
-        return f'{node.dest} = {node.left} + {node.right}'
+    def visit(self, node: PlusNode):  # noqa:F811
+        return f"{node.dest} = {node.left} + {node.right}"
 
     @when(MinusNode)
-    def visit(self, node: MinusNode):
-        return f'{node.dest} = {node.left} - {node.right}'
+    def visit(self, node: MinusNode):  # noqa:F811
+        return f"{node.dest} = {node.left} - {node.right}"
 
     @when(StarNode)
-    def visit(self, node: StarNode):
-        return f'{node.dest} = {node.left} * {node.right}'
+    def visit(self, node: StarNode):  # noqa:F811
+        return f"{node.dest} = {node.left} * {node.right}"
 
     @when(DivNode)
-    def visit(self, node: DivNode):
-        return f'{node.dest} = {node.left} / {node.right}'
+    def visit(self, node: DivNode):  # noqa:F811
+        return f"{node.dest} = {node.left} / {node.right}"
 
     @when(AllocateNode)
-    def visit(self, node: AllocateNode):
-        return f'{node.dest} = ALLOCATE {node.type}'
+    def visit(self, node: AllocateNode):  # noqa:F811
+        return f"{node.dest} = ALLOCATE {node.type}"
 
     @when(TypeOfNode)
-    def visit(self, node: TypeOfNode):
-        return f'{node.dest} = TYPEOF {node.obj}'
+    def visit(self, node: TypeOfNode):  # noqa:F811
+        return f"{node.dest} = TYPEOF {node.obj}"
 
     @when(StaticCallNode)
-    def visit(self, node: StaticCallNode):
-        return f'{node.dest} = CALL {node.function}'
+    def visit(self, node: StaticCallNode):  # noqa:F811
+        return f"{node.dest} = CALL {node.function}"
 
     @when(DynamicCallNode)
-    def visit(self, node: DynamicCallNode):
-        return f'{node.dest} = VCALL {node.type} {node.method}'
+    def visit(self, node: DynamicCallNode):  # noqa:F811
+        return f"{node.dest} = VCALL {node.type} {node.method}"
 
     @when(ArgNode)
-    def visit(self, node: ArgNode):
-        return f'ARG {node.name}'
+    def visit(self, node: ArgNode):  # noqa:F811
+        return f"ARG {node.name}"
 
     @when(CleanArgsNode)
-    def visit(self, node:CleanArgsNode):
-        return f'CLEANARG {node.nargs}'
+    def visit(self, node: CleanArgsNode):  # noqa:F811
+        return f"CLEANARG {node.nargs}"
 
     @when(ReturnNode)
-    def visit(self, node: ReturnNode):
+    def visit(self, node: ReturnNode):  # noqa:F811
         return f'RETURN {node.value if node.value is not None else ""}'
 
     @when(ReadIntNode)
-    def visit(self, node: ReadIntNode):
-        return f'{node.dest} = READINT'
+    def visit(self, node: ReadIntNode):  # noqa:F811
+        return f"{node.dest} = READINT"
 
     @when(ReadStrNode)
-    def visit(self, node: ReadStrNode):
-        return f'{node.dest} = READSTR'
+    def visit(self, node: ReadStrNode):  # noqa:F811
+        return f"{node.dest} = READSTR"
 
     @when(PrintIntNode)
-    def visit(self, node: PrintIntNode):
-        return f'PRINTINT {node.str_addr}'
+    def visit(self, node: PrintIntNode):  # noqa:F811
+        return f"PRINTINT {node.str_addr}"
 
     @when(PrintStrNode)
-    def visit(self, node: PrintStrNode):
-        return f'PRINTSTR {node.str_addr}'
+    def visit(self, node: PrintStrNode):  # noqa:F811
+        return f"PRINTSTR {node.str_addr}"
 
     @when(LengthNode)
-    def visit(self, node: LengthNode):
-        return f'{node.dest} = LENGTH {node.msg}'
-    
+    def visit(self, node: LengthNode):  # noqa:F811
+        return f"{node.dest} = LENGTH {node.msg}"
+
     @when(ConcatNode)
-    def visit(self, node: ConcatNode):
-        return f'{node.dest} = CONCAT {node.msg1} {node.msg2}'
+    def visit(self, node: ConcatNode):  # noqa:F811
+        return f"{node.dest} = CONCAT {node.msg1} {node.msg2}"
 
     @when(PrefixNode)
-    def visit(self, node: PrefixNode):
-        return f'{node.dest} = PREFIX {node.msg1} {node.msg2}'
+    def visit(self, node: PrefixNode):  # noqa:F811
+        return f"{node.dest} = PREFIX {node.msg1} {node.msg2}"
 
     @when(SubstringNode)
-    def visit(self, node: SubstringNode):
-        return f'{node.dest} = SUBSTRING {node.msg1} {node.start} {node.length}'
+    def visit(self, node: SubstringNode):  # noqa:F811
+        result = f"{node.dest} = SUBSTRING {node.msg1} "
+        result += f"{node.start} {node.length}"
+        return result
 
     @when(StringEqualNode)
-    def visit(self, node: StringEqualNode):
-        return f'{node.dest} = STREQ {node.msg1} {node.msg2}'
+    def visit(self, node: StringEqualNode):  # noqa:F811
+        return f"{node.dest} = STREQ {node.msg1} {node.msg2}"
 
     @when(GetAttribNode)
-    def visit(self, node: GetAttribNode):
-        return f'{node.dest} = GETATTR {node.obj} {node.attrib}'
+    def visit(self, node: GetAttribNode):  # noqa:F811
+        return f"{node.dest} = GETATTR {node.obj} {node.attrib}"
 
     @when(SetAttribNode)
-    def visit(self, node: SetAttribNode):
-        return f'SETATTR {node.obj} {node.attrib} {node.value}'
+    def visit(self, node: SetAttribNode):  # noqa:F811
+        return f"SETATTR {node.obj} {node.attrib} {node.value}"
 
     @when(LabelNode)
-    def visit(self, node: LabelNode):
-        return f'LABEL {node.label}'
+    def visit(self, node: LabelNode):  # noqa:F811
+        return f"LABEL {node.label}"
 
     @when(GotoNode)
-    def visit(self, node: GotoNode):
-        return f'GOTO {node.label}'
+    def visit(self, node: GotoNode):  # noqa:F811
+        return f"GOTO {node.label}"
 
     @when(GotoIfNode)
-    def visit(self, node: GotoIfNode):
-        return f'IF {node.value} GOTO {node.label}'
+    def visit(self, node: GotoIfNode):  # noqa:F811
+        return f"IF {node.value} GOTO {node.label}"
