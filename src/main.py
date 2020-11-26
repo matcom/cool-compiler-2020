2c9417c9edd587e1f8a5872fc51e634933dd5a20
rmarticedeno <rmarticedeno@gmail.com>
2020-11-25 22:06:25 -0500

    temporal changes

--- src/main.py
+++ src/main.py
@@ -28,24 +28,32 @@ parse, operations = CoolParser(tokens)
 
 ast = evaluate_reverse_parse(parse,operations,tokens)
 
-collect = Collector()
+collect_errors = []
+collect = Collector(collect_errors)
 collect.visit(ast)
 context = collect.context
-builder = Builder(context)
-builder.visit(ast)
-context = builder.context
-checker = Checker(context)
-scope = checker.visit(ast)
+builder_errors = []
+#builder = Builder(context, builder_errors)
+#builder.visit(ast)
+#context = builder.context
+#checker_errors = []
+#checker = Checker(context, checker_errors)
+#scope = checker.visit(ast)
 
 
-cil = COOL_TO_CIL(checker.context)
-# cil = COOL_TO_CIL_VISITOR(checker.context)
-# sc = Scope()
-cil_ast = cil.visit(ast,scope)
-# f_ast = Format().visit(ast)
-f_ast = CIL_FORMATTER().visit(cil_ast)
-string_formatted = str(f_ast)
-output_file.write(string_formatted)
+# cil = COOL_TO_CIL(checker.context)
+# # cil = COOL_TO_CIL_VISITOR(checker.context)
+# # sc = Scope()
+# cil_ast = cil.visit(ast,scope)
+# # f_ast = Format().visit(ast)
+# f_ast = CIL_FORMATTER().visit(cil_ast)
+# string_formatted = str(f_ast)
+#output_file.write(string_formatted)
+output_file.write(str(collect_errors))
+#print(str(collect_errors))
+#output_file.write(str(builder_errors))
+#output_file.write(str(checker_errors))
+#output_file.write(collect_errors)
 output_file.close()
 
 
@@ -54,4 +62,12 @@ if not operations:
     print(SyntacticError(parse.line,parse.column, message))
     exit(1)
 #print(parse)
+
+
+if len(collect_errors):
+    for e in collect_errors:
+        print(e)
+    print('/n')
+    exit(1)
+
 exit(0)
