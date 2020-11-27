4b746b2266641bd743155bf9ff41de1a3f40eee9
leynier <leynier41@gmail.com>
2020-11-27 02:32:18 -0500

    Fix tests

--- tests/codegen_test.py
+++ tests/codegen_test.py
@@ -2,11 +2,10 @@ import os
 
 import pytest
 
-from utils import compare_errors  # type: ignore
+from utils import compare_outputs  # type: ignore
 
 tests_dir = __file__.rpartition("/")[0] + "/codegen/"
 tests = [(file) for file in os.listdir(tests_dir) if file.endswith(".cl")]
-
 
 # @pytest.mark.lexer
 # @pytest.mark.parser
@@ -16,4 +15,9 @@ tests = [(file) for file in os.listdir(tests_dir) if file.endswith(".cl")]
 @pytest.mark.run(order=4)
 @pytest.mark.parametrize("cool_file", tests)
 def test_codegen(compiler_path, cool_file):
-    compare_errors(compiler_path, tests_dir + cool_file, None)
+    compare_outputs(
+        compiler_path,
+        tests_dir + cool_file,
+        tests_dir + cool_file[:-3] + "_input.txt",
+        tests_dir + cool_file[:-3] + "_output.txt",
+    )
--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -1,15 +1,13 @@
-import subprocess
 import re
-
+import subprocess
 
 COMPILER_TIMEOUT = "El compilador tarda mucho en responder."
+SPIM_TIMEOUT = "El spim tarda mucho en responder."
 TEST_MUST_FAIL = "El test %s debe fallar al compilar"
 TEST_MUST_COMPILE = "El test %s debe compilar"
-BAD_ERROR_FORMAT = "El error no esta en formato: (<línea>,<columna>) - "
-BAD_ERROR_FORMAT += "<tipo_de_error>: <texto_del_error> o no se encuentra "
-BAD_ERROR_FORMAT += "en la 3ra linea\n\n%s"
-UNEXPECTED_ERROR = "Se esperaba un %s en (%d, %d). "
-UNEXPECTED_ERROR += "Su error fue un %s en (%d, %d)"
+BAD_ERROR_FORMAT = """El error no esta en formato: (<línea>,<columna>) - <tipo_de_error>: <texto_del_error>
+                        o no se encuentra en la 3ra linea\n\n%s"""
+UNEXPECTED_ERROR = "Se esperaba un %s en (%d, %d). Su error fue un %s en (%d, %d)"
 UNEXPECTED_OUTPUT = "La salida de %s no es la esperada:\n%s\nEsperada:\n%s"
 
 ERROR_FORMAT = r"^\s*\(\s*(\d+)\s*,\s*(\d+)\s*\)\s*-\s*(\w+)\s*:(.*)$"
@@ -29,14 +27,7 @@ def first_error(compiler_output: list, errors: list):
 
     assert (
         line == oline and column == ocolumn and error_type == oerror_type
-    ), UNEXPECTED_ERROR % (
-        error_type,
-        line,
-        column,
-        oerror_type,
-        oline,
-        ocolumn,
-    )
+    ), UNEXPECTED_ERROR % (error_type, line, column, oerror_type, oline, ocolumn)
 
 
 def first_error_only_line(compiler_output: list, errors: list):
@@ -56,7 +47,7 @@ def first_error_only_line(compiler_output: list, errors: list):
 
 def get_file_name(path: str):
     try:
-        return path[path.rindex("/") + 1 :]  # noqa
+        return path[path.rindex("/") + 1 :]
     except ValueError:
         return path
 
@@ -78,19 +69,64 @@ def compare_errors(
     except subprocess.TimeoutExpired:
         assert False, COMPILER_TIMEOUT
 
+    assert return_code == 1, TEST_MUST_FAIL % get_file_name(cool_file_path)
+
+    fd = open(error_file_path, "r")
+    errors = fd.read().split("\n")
+    fd.close()
+
+    # checking the errors of compiler
     compiler_output = output.split("\n")
+    cmp(compiler_output[2:], errors)
 
-    if error_file_path:
-        assert return_code == 1, TEST_MUST_FAIL % get_file_name(cool_file_path)
 
-        fd = open(error_file_path, "r")
-        errors = fd.read().split("\n")
-        fd.close()
+SPIM_HEADER = r"""^SPIM Version .+ of .+
+Copyright .+\, James R\. Larus\.
+All Rights Reserved\.
+See the file README for a full copyright notice\.
+(?:Loaded: .+\n)*"""
 
-        # checking the errors of compiler
-        cmp(compiler_output[2:], errors)
-    else:
-        print(return_code, output)
-        assert return_code == 0, TEST_MUST_COMPILE % get_file_name(
-            cool_file_path,
+
+def compare_outputs(
+    compiler_path: str,
+    cool_file_path: str,
+    input_file_path: str,
+    output_file_path: str,
+    timeout=100,
+):
+    try:
+        sp = subprocess.run(
+            ["bash", compiler_path, cool_file_path],
+            capture_output=True,
+            timeout=timeout,
         )
+        assert sp.returncode == 0, TEST_MUST_COMPILE % get_file_name(cool_file_path)
+    except subprocess.TimeoutExpired:
+        assert False, COMPILER_TIMEOUT
+
+    spim_file = cool_file_path[:-2] + "mips"
+
+    try:
+        fd = open(input_file_path, "rb")
+        sp = subprocess.run(
+            ["spim", "-file", spim_file],
+            input=fd.read(),
+            capture_output=True,
+            timeout=timeout,
+        )
+        fd.close()
+        mo = re.match(SPIM_HEADER, sp.stdout.decode())
+        if mo:
+            output = mo.string[mo.end() :]
+    except subprocess.TimeoutExpired:
+        assert False, SPIM_TIMEOUT
+
+    fd = open(output_file_path, "r")
+    eoutput = fd.read()
+    fd.close()
+
+    assert output == eoutput, UNEXPECTED_OUTPUT % (
+        spim_file,
+        repr(output),
+        repr(eoutput),
+    )
