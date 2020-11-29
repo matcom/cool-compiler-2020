28c8d9a28d65c9c77b1a2276bdb131a5b1a60a76
Al3ERT <58373650+Al3ERT@users.noreply.github.com>
2020-11-29 13:47:12 -0500

    Probando 5

--- src/TigerandButiCompiler.py
+++ src/TigerandButiCompiler.py
@@ -693,6 +693,7 @@ if __name__=='__main__':
             instrucciones=toMIPS.visit(codigoCIL, None)
             with open(f'{sys.argv[1][:-3]}.mips', 'w') as f:
                 f.write(instrucciones)
+            exit(1)
         else:
             exit(1)
     else:
