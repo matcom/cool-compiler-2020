535d4ede1d955600c3635e18a36315d9db956e50
Pablo Antonio de Armas Suárez <paas0725@gmail.com>
2020-11-29 13:43:51 -0500

    Prueba para ver por qué los test fallan en GitHub (3)

--- src/COOLCompiler.py
+++ src/COOLCompiler.py
@@ -15,6 +15,10 @@ from NoTabsFileStream import NoTabsFileStream
 
 
 def main(argv):
+    print("COOLCompiler 1.0.3")
+    print("Copyright (C) 2019-2020: Liset Silva Oropesa, Pablo A. de Armas Suárez, Yenli Gil Machado")
+    print("3ra linea")
+
     if len(argv) < 2:
         print("ERROR: no input filename")
         return sys.exit(errno.EPERM)
--- src/coolc.sh
+++ src/coolc.sh
@@ -4,9 +4,6 @@ INPUT_FILE=$1
 OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
-echo "COOLCompiler 1.0.2"
-echo "Copyright (C) 2019-2020: Liset Silva Oropesa, Pablo A. de Armas Suárez, Yenli Gil Machado"
-echo "3ra línea"
 
 # Llamar al compilador
 #echo "Compiling $INPUT_FILE into $OUTPUT_FILE"
