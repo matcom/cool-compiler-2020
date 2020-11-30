08563259f04392fc610d20fe6c82fe7cbe70f97a
Pablo Antonio de Armas Suárez <paas0725@gmail.com>
2020-11-29 22:17:21 -0500

    Pruebas para encontrar el problema de los test en GitHub (2)

--- src/MockCompiler.py
+++ src/MockCompiler.py
@@ -1,9 +1,20 @@
 import sys
 import errno
+import os.path
 
 def main(argv):
     print("COOLCompiler 1.0.3")
     print("Copyright (C) 2019-2020: Liset Silva Oropesa, Pablo A. de Armas Suárez, Yenli Gil Machado")
+
+    if len(argv) < 2:
+        print("ERROR: no input filename")
+        return sys.exit(errno.EPERM)
+
+    if not os.path.isfile(argv[1]):
+        print("ERROR: invalid input filename: " + argv[1])
+        return sys.exit(errno.EPERM)
+
+    input = NoTabsFileStream(argv[1])
 
     print("3rd line")
 
