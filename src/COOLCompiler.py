779795a8c4abb7cdf2e3451e0bdc551e353235cd
Pablo Antonio de Armas Suárez <paas0725@gmail.com>
2020-11-29 23:00:00 -0500

    Pruebas para encontrar el problema de los test en GitHub (7)

--- src/COOLCompiler.py
+++ src/COOLCompiler.py
@@ -24,39 +24,40 @@ def main(argv):
         print("ERROR: invalid input filename: " + argv[1])
         return sys.exit(errno.EPERM)
 
-    input = FileStream(argv[1], "utf-8")
-    lexer = COOLLexer(input)
-    lexer.removeErrorListeners()
-    lexer.addErrorListener(COOLLexerErrorListener())
-    token = lexer.nextToken()
-    while token.type != Token.EOF:
-        #print(token.type)
-        token = lexer.nextToken()
-    if lexer.hasErrors:
-        return sys.exit(errno.EPERM)
-    lexer.reset()
-    stream = CommonTokenStream(lexer)
-    parser = COOLParser(stream)
-    parser.removeErrorListeners()
-    parser.addErrorListener(COOLParserErrorListener())
-    tree = parser.program()
-    if parser.getNumberOfSyntaxErrors() > 0:
-        return sys.exit(errno.EPERM)
-    visitor = TypeCOOLVisitor()
-    visitor.visitProgram(tree, argv[1])
-    typeTree = visitor.TypeTable
-    consTble = visitor.ConstantTable
-    semanticAnalizer = SemanticCOOLVisitor(typeTree)
-    codegenerator = CodegenVisitor(typeTree, consTble, visitor.Counter)
-    semanticAnalizer.visitProgram(tree)
-    if semanticAnalizer.hasNoError:
-        if len(argv) > 2:
-            outFilename = argv[2]
-        else:
-            outFilename = os.path.splitext(argv[1])[0] + ".s"
-        codegenerator.visitProgram(tree, outFilename)
-    else:
-        return sys.exit(errno.EPERM)
+
+    # input = FileStream(argv[1], "utf-8")
+    # lexer = COOLLexer(input)
+    # lexer.removeErrorListeners()
+    # lexer.addErrorListener(COOLLexerErrorListener())
+    # token = lexer.nextToken()
+    # while token.type != Token.EOF:
+    #     #print(token.type)
+    #     token = lexer.nextToken()
+    # if lexer.hasErrors:
+    #     return sys.exit(errno.EPERM)
+    # lexer.reset()
+    # stream = CommonTokenStream(lexer)
+    # parser = COOLParser(stream)
+    # parser.removeErrorListeners()
+    # parser.addErrorListener(COOLParserErrorListener())
+    # tree = parser.program()
+    # if parser.getNumberOfSyntaxErrors() > 0:
+    #     return sys.exit(errno.EPERM)
+    # visitor = TypeCOOLVisitor()
+    # visitor.visitProgram(tree, argv[1])
+    # typeTree = visitor.TypeTable
+    # consTble = visitor.ConstantTable
+    # semanticAnalizer = SemanticCOOLVisitor(typeTree)
+    # codegenerator = CodegenVisitor(typeTree, consTble, visitor.Counter)
+    # semanticAnalizer.visitProgram(tree)
+    # if semanticAnalizer.hasNoError:
+    #     if len(argv) > 2:
+    #         outFilename = argv[2]
+    #     else:
+    #         outFilename = os.path.splitext(argv[1])[0] + ".s"
+    #     codegenerator.visitProgram(tree, outFilename)
+    # else:
+    #     return sys.exit(errno.EPERM)
 
     return 0
 
