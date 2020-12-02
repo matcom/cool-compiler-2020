f2ddf13db8994b123d0a501ea760b715e480bd64
Karl <karlsosa4298@gmail.com>
2020-12-01 20:49:18 -0500

    typo2

--- src/mips.py
+++ src/mips.py
@@ -564,7 +564,8 @@ function_in_string_at_IO:
 
             addiu $t0, $t0, -2
             li t1 10
-            bne $t1 , $t0 not_slash
+            lb $t2, ($t0)
+            bne $t1 , $t2 not_slash
             sb $zero, ($t0)
 
             :not_slash
