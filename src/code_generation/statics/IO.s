9837498ebb0d2ede07337e039d288cda47f94daa
CRafa97 <c.ortega@estudiantes.matcom.uh.cu>
2020-12-04 17:29:01 -0500

    working in static methods

--- src/code_generation/statics/IO.s
+++ src/code_generation/statics/IO.s
@@ -8,46 +8,60 @@ jr $ra
 
 
 IO.out_int:
+sw $ra, 0($sp)
+addiu $sp, $sp, -4
 li $v0, 1
-lw $a0, -4($sp)
+lw $a0, 8($sp)
 syscall
 jr $ra
 
 
 IO.in_string:
-move $a3, $ra
+move $fp, $sp
+sw $ra, 0($sp)
+addiu $sp, $sp, -4
 la $a0, buffer
-li $a1, 65536
+li $a1, 1024
 li $v0, 8
 syscall
-sw $a1, 0($sp)
+sw $fp, 0($sp)
 addiu $sp, $sp, -4
 sw $a0, 0($sp)
+addiu $sp, $sp, -4
 jal String.length
-addiu $sp, $sp, 4
-move $a2, $sp
-addiu $a2, $a2, -1
-move $a0, $v0
+addiu $a0, $a0, 1
 li $v0, 9
 syscall
-move $v1, $v0
+move $t0, $v0
 la $a0, buffer
-li $a2, 0
-_in_string.loop:
-beqz $a2, _in_string.end
-lb $a1, 4($sp)
-sb $a1, 4($sp)
+istr_copy:
+lb $t1, ($a0)
+sb $t1, ($t0)
 addiu $a0, $a0, 1
-addiu $v1, $v1, 1
-addiu $a2, $a2, -1
-j _in_string.loop
-_in_string.end:
-sb $zero, 0($sp)
-move $ra, $a3
-jr $ra
+addiu $t0, $t0, 1
+bne $t1, $zero, istr_copy
+addiu $t0, $t0, -2
+li $t1 10
+lb $t2, ($t0)
+bne $t1 , $t2 not_slash
+sb $zero, ($t0)
+not_slash:
+move $a0, $v0
+lw $ra, 0($fp)
+addiu $sp, $sp, 12
+lw $fp, 0($sp)
+jr $ra 
 
 
 IO.in_int:
+move $fp, $sp
+sw $ra, 0($sp)
+addiu $sp, $sp, -4
+lw $a0, 4($fp)
 li $v0, 5
 syscall
+move $a0, $v0
+lw $ra, 0($fp)
+addiu $sp, $sp, 12
+lw $fp, 0($sp)
 jr $ra
