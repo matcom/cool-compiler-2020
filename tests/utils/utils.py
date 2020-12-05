a46f9efe34d6c8bac5a9dfe616ec9e4572ac0c4b
Marcos Antonio Maceo Reyes <marcosmaceowork@gmail.com>
2020-12-05 11:56:10 -0500

    Refactor error print

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -57,9 +57,9 @@ def compare_errors(compiler_path: str, cool_file_path: str, error_file_path: str
     errors = fd.read().split('\n')
     fd.close()
 
-    print(compiler_output)
     # checking the errors of compiler
     compiler_output = output.split('\n')
+    print(compiler_output)
     cmp(compiler_output[2:], errors)
 
 SPIM_HEADER = r'''^SPIM Version .+ of .+
