5e37b847492b9e8d8d7afa0084cdf5a569b76249
lagcleaner <l.garcia3@estudiantes.matcom.uh.cu>
2020-12-11 12:08:13 -0500

    inheritance build in collector and store in context

--- src/engine/visitors/collector.py
+++ src/engine/visitors/collector.py
@@ -2,21 +2,22 @@ from engine.cp import SemanticError, visitor, Context, SelfType
 from engine.parser import ProgramNode, ClassDeclarationNode
 from engine.semantic_errors import ERROR_ON_LN_COL, CYCLIC_HERITAGE
 
+
 class Collector:
-    def __init__(self, errors = []):
+    def __init__(self, errors=[]):
         self.context = Context()
         self.errors = errors
         self.parents = {}
 
-        #Tipos especiales
+        # Tipos especiales
         self.context.add_type(SelfType())
 
-        #Tipos Buit-In
-        self.context.create_type('Object',builtin = True)
-        self.context.create_type('IO',builtin = True)
-        self.context.create_type('Int',builtin = True)
-        self.context.create_type('String',builtin = True)
-        self.context.create_type('Bool',builtin = True)
+        # Tipos Buit-In
+        self.context.create_type('Object', builtin=True)
+        self.context.create_type('IO', builtin=True)
+        self.context.create_type('Int', builtin=True)
+        self.context.create_type('String', builtin=True)
+        self.context.create_type('Bool', builtin=True)
 
         self.parents['Object'] = None
         self.parents['IO'] = 'Object'
@@ -24,17 +25,21 @@ class Collector:
         self.parents['String'] = 'Object'
         self.parents['Bool'] = 'Object'
 
+    @property
+    def parents(self):
+        return self.context.inheritance
+
     def ciclic_heritage(self, name, parent):
         if parent is None:
             return False
         if name == parent:
             return True
         return self.ciclic_heritage(name, self.parents[parent])
-    
+
     @visitor.on('node')
     def visit(self, node):
         pass
-        
+
     @visitor.when(ProgramNode)
     def visit(self, node):
         for class_def in node.declarations:
@@ -45,12 +50,14 @@ class Collector:
                 if declaration.id.lex not in self.parents.keys():
                     self.parents[declaration.id.lex] = 'Object' if declaration.parent == None else declaration.parent.lex
                 if declaration.parent != None and declaration.parent.lex in self.parents.keys() and self.ciclic_heritage(declaration.id.lex, declaration.parent.lex):
-                    self.errors.append(ERROR_ON_LN_COL % (declaration.line, declaration.column) + "SemanticError: " + CYCLIC_HERITAGE % (declaration.id.lex))
+                    self.errors.append(ERROR_ON_LN_COL % (
+                        declaration.line, declaration.column) + "SemanticError: " + CYCLIC_HERITAGE % (declaration.id.lex))
                     break
-    
+
     @visitor.when(ClassDeclarationNode)
     def visit(self, node):
         try:
             self.context.create_type(node.id.lex)
         except SemanticError as se:
-            self.errors.append(ERROR_ON_LN_COL % (node.line, node.column) + "SemanticError: " + se.text)
+            self.errors.append(ERROR_ON_LN_COL % (
+                node.line, node.column) + "SemanticError: " + se.text)
