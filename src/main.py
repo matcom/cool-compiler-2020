e7e5b21574ac6b467538bc77e5f78be6d42c7284
Roberto Marti Cedeño <rmarticedeno@gmail.com>
2020-12-10 22:32:29 -0500

    Fix wrong semantic test

--- src/main.py
+++ src/main.py
@@ -42,17 +42,19 @@ collect = Collector(collect_errors)
 collect.visit(ast)
 
 if len(collect_errors):
-    for e in collect_errors[::-1]:
+    for e in collect_errors:
         print(e)
     exit(1)
 
 context = collect.context
 builder_errors = []
 builder = Builder(context, builder_errors)
+
+
 builder.visit(ast)
 
 if len(builder_errors):
-    for e in builder_errors[::-1]:
+    for e in builder_errors:
         print(e)
     exit(1)
 
