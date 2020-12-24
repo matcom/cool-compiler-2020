f12660e1e96e8bc9d8a4fca4dffbd3309dfd46dd
ezielramos <e.ramos@estudiantes.matcom.uh.cu>
2020-12-24 18:12:54 -0500

    5

--- src/compiling.py
+++ src/compiling.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import sys, lexerTest, parserTest
-=======
-import sys, lexerTest, parserTest, semanticTest
->>>>>>> semantic_work
-=======
-import sys, lexerTest, parserTest
->>>>>>> semantic_work
 
 addr = None
 addr = sys.argv[1]
