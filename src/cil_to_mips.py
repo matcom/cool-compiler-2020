1e7cfbeea3c04f9edd03f250077398af492b214d
ezielramos <47543896+ezielramos@users.noreply.github.com>
2021-02-11 00:01:32 -0500

    Update cil_to_mips.py

--- src/cil_to_mips.py
+++ src/cil_to_mips.py
@@ -26,7 +26,8 @@ class Build_Mips:
         for _str, tag in program.data_section.items():
             self.add(tag + ':' + ' .asciiz ' + _str)
         self.add('.text')
-        self.add('entry:')
+        self.add('.global __start')
+        self.add('__start:')
         self.add('jal function_Main_main')
         self.add('li $v0, 10') #exit()
         self.add('syscall')
